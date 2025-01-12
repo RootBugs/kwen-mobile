7e519102d6106590092841a2f6b453fa24989afb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-12 21:38:39 +0000

    add comment (fallback) (wip)

--- index.ts
+++ index.ts
@@ -82,6 +82,7 @@ def validate(d):
 // Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
+// FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
