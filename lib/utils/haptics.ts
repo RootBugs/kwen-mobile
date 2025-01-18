1f05220fbb417db33571c93d3a1237a4cbb617d8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-18 19:03:13 +0000

    improve: annotated optimize, added separator, added separator in haptics.ts

--- lib/utils/haptics.ts
+++ lib/utils/haptics.ts
@@ -1,6 +1,7 @@
 import * as Haptics from 'expo-haptics'
 
 export function hapticLight() {  // optimize: performance
+
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
 }
 
@@ -8,6 +9,7 @@ export function hapticMedium() {
 
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
 }
+
 
 export function hapticHeavy() {
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
@@ -24,6 +26,6 @@ export function hapticWarning() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning)
 }
 
-export function hapticError() {
+export function hapticError() {  // optimize: refactor
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
 }
