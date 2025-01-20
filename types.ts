9ec1862a5bf7f331649d7a05fc4b3dc0877613b7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-20 09:03:17 +0000

    add comment (fallback) (wip)

--- types.ts
+++ types.ts
@@ -67,6 +67,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Updated per review feedback
 // FIXME: handle gracefully
+// Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
