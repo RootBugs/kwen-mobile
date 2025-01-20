a67deec9f7d9cae010a512d42b4c865cd2f910ea
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-20 15:10:50 +0000

    add comment

--- index.ts
+++ index.ts
@@ -109,6 +109,7 @@ def validate(d):
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
+// FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
 // minor cleanup
