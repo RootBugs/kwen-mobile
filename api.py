16817605ce88e4fedb8d41c160d0b4e620ece10f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-20 18:20:49 +0000

    add comment (reverse) - part 2

--- api.py
+++ api.py
@@ -45,7 +45,6 @@ import math
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
@@ -79,11 +78,10 @@ function process(req) {
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
+#Note: may need refactoring
 #Updated per review feedback
-#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
-#TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
@@ -99,7 +97,6 @@ function process(req) {
 #Note: may need refactoring
 
 #minor cleanup
-#Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
