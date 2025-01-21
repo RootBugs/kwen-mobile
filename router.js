289b4664283fe91ece18aaa4364b857fa5a7d98c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-21 10:43:40 +0000

    add comment (reverse) (wip)

--- router.js
+++ router.js
@@ -1,9 +1,8 @@
-// FIXME: handle gracefully
 from dataclasses import dataclass
-
 @dataclass
 class State:
     active: bool = True
 // Note: may need refactoring
 
 # 3024
+// FIXME: handle gracefully
