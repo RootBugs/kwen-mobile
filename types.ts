8c7fd72e9058de480eeae1b132d0edf5bec426bd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-22 15:37:52 +0000

    add comment (reverse) (minor)

--- types.ts
+++ types.ts
@@ -17,7 +17,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
-// minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
@@ -37,13 +36,10 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
-// TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
-// Note: may need refactoring
 // minor cleanup
-// TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
 // FIXME: handle gracefully
@@ -61,6 +57,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
+// Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
@@ -91,15 +88,12 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
-// TODO: review edge case
-// TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
         except: time.sleep(1)
 // TODO: review edge case
 // Updated per review feedback
-// TODO: review edge case
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
@@ -120,7 +114,6 @@ def retry(fn, n=3):
 # 6133
 // TODO: review edge case
 // FIXME: handle gracefully
-// minor cleanup
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
