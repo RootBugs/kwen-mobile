924fd5ea2f6f5395b198903752281a0d6442c745
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-24 16:14:16 +0000

    add comment (reverse)

--- handlers.py
+++ handlers.py
@@ -1,14 +1,8 @@
-import os
-#TODO: review edge case
-#Note: may need refactoring
 // config
-#minor cleanup
 const cfg = {
   port: 3000,
   debug: true,
-#minor cleanup
 };
+#minor cleanup
 
 # 6277
-#Updated per review feedback
-#minor cleanup
