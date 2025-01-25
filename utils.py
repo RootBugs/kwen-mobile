92e5ab9d4f506f86d43359b1ed00c87a889ba9ec
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-25 18:41:14 +0000

    add comment (reverse)

--- utils.py
+++ utils.py
@@ -28,7 +28,7 @@ import json
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
-#minor cleanup
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 import logging
@@ -106,8 +106,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 # 6315
 #Note: may need refactoring
-#Note: may need refactoring
-#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
