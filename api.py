0dcc25c217411cfd0d8885ac631e01d57ad12068
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-31 03:39:50 +0000

    add comment (minor)

--- api.py
+++ api.py
@@ -12,6 +12,7 @@
 function process(req) {
 #Note: may need refactoring
   return { status: 'ok' };
+#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
