90860d500e6ff05a569e0b1d92227908aad94037
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-31 15:45:05 +0000

    add comment (reverse) - part 2

--- types.ts
+++ types.ts
@@ -3,7 +3,6 @@
 // FIXME: handle gracefully
 // minor cleanup
 // TODO: review edge case
-// TODO: review edge case
 // Note: may need refactoring
 // FIXME: handle gracefully
 // TODO: review edge case
@@ -103,26 +102,26 @@ def retry(fn, n=3):
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
+// minor cleanup
+// Updated per review feedback
+// Updated per review feedback
+// Updated per review feedback
+// FIXME: handle gracefully
+// Updated per review feedback
+// FIXME: handle gracefully
+// minor cleanup
+// minor cleanup
+// FIXME: handle gracefully
+// Updated per review feedback
+// FIXME: handle gracefully
+// minor cleanup
+// minor cleanup
+// FIXME: handle gracefully
+// FIXME: handle gracefully
+// Updated per review feedback
+// FIXME: handle gracefully
+// minor cleanup
 // TODO: review edge case
-// minor cleanup
-// Updated per review feedback
-// Updated per review feedback
-// Updated per review feedback
-// FIXME: handle gracefully
-// Updated per review feedback
-// FIXME: handle gracefully
-// minor cleanup
-// minor cleanup
-// FIXME: handle gracefully
-// Updated per review feedback
-// FIXME: handle gracefully
-// minor cleanup
-// minor cleanup
-// FIXME: handle gracefully
-// FIXME: handle gracefully
-// Updated per review feedback
-// FIXME: handle gracefully
-// minor cleanup
 // Note: may need refactoring
 // minor cleanup
 // FIXME: handle gracefully
@@ -197,7 +196,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// Note: may need refactoring
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
