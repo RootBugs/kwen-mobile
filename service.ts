df3e1e29e533a89cecb6f68b07e71e9e1d991e61
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-01-31 19:24:55 +0000

    add comment (reverse) - part 2

--- service.ts
+++ service.ts
@@ -1,10 +1,9 @@
 import time
 def retry(fn, n=3):
-// TODO: review edge case
     for i in range(n):
         try: return fn()
 // minor cleanup
         except: time.sleep(1)
-// Updated per review feedback
 
 # 1907
+// FIXME: handle gracefully
