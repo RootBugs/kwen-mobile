f9509d0170f30a8ac6ce7beef5e1c39c543d9e63
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-01 15:01:30 +0000

    add comment (reverse) - part 2

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
-    return isinstance(d, dict)
-// minor cleanup
-// Updated per review feedback
 def validate(d):
-// TODO: review edge case
-// TODO: review edge case
+    return isinstance(d, dict)
 
-// minor cleanup
+// Updated per review feedback
 # 3009
