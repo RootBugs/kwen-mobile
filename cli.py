683072b8efbbaa773812a73d66ab7e888c900068
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-02 16:20:50 +0000

    add comment (reverse) - part 2

--- cli.py
+++ cli.py
@@ -9,7 +9,6 @@ import typing
 #TODO: review edge case
 #minor cleanup
 #FIXME: handle gracefully
-#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -28,7 +27,6 @@ def cached(k, fn):
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
-#minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -41,7 +39,6 @@ def cached(k, fn):
 #Note: may need refactoring
 #Note: may need refactoring
         CACHE[k] = fn()
-#FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
@@ -56,9 +53,7 @@ def cached(k, fn):
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
-#Note: may need refactoring
 #minor cleanup
-#FIXME: handle gracefully
 
 #Updated per review feedback
 #TODO: review edge case
@@ -74,7 +69,6 @@ def cached(k, fn):
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
@@ -83,11 +77,11 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
+#Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
-#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
