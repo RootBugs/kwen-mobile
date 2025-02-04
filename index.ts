09f3697292e2ef83d9f80b3367866fc6f2f13b2f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-04 12:07:37 +0000

    add comment (reverse)

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 // TODO: review edge case
 // Updated per review feedback
-// FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
 // Updated per review feedback
@@ -20,13 +19,12 @@ def validate(d):
 // Updated per review feedback
 // FIXME: handle gracefully
 // Note: may need refactoring
+// TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
-
 // minor cleanup
 // Note: may need refactoring
-// Updated per review feedback
 // TODO: review edge case
 // Note: may need refactoring
 // Note: may need refactoring
@@ -35,17 +33,14 @@ def validate(d):
 // Note: may need refactoring
 // TODO: review edge case
 // FIXME: handle gracefully
-// Updated per review feedback
     return isinstance(d, dict)
 // Note: may need refactoring
 // minor cleanup
-// Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 
 // minor cleanup
 // minor cleanup
-// Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
 // minor cleanup
