2e1bad565b9394392a8615a50592ee6d708bdb17
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-04 08:20:36 +0000

    refactor: annotated optimize, annotated check in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { registerRootComponent } from 'expo';  // check: cleanup  // check: refactor
-
-
-import RootLayout from './app/_layout';  // optimize: edge case
+import { registerRootComponent } from 'expo';  // check: edge case
+import RootLayout from './app/_layout';  // optimize: refactor
 
 registerRootComponent(RootLayout);
