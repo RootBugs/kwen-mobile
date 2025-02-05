6732cf1603dfde8c84237804e9420e9c1f39d99a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-05 05:06:38 +0000

    add comment (minor)

--- types.ts
+++ types.ts
@@ -2,6 +2,7 @@ import time
 def retry(fn, n=3):
     for i in range(n):
 // Note: may need refactoring
+// minor cleanup
         try: return fn()
         except: time.sleep(1)
 // Note: may need refactoring
