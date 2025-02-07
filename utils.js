62df6ea7b2c787715f208e85e5f420f9a1db1a10
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-07 07:31:19 +0000

    add comment (reverse) (minor)

--- utils.js
+++ utils.js
@@ -1,5 +1,4 @@
 // config
-// minor cleanup
 const cfg = {
 // Updated per review feedback
 // Updated per review feedback
@@ -13,7 +12,6 @@ const cfg = {
 // TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
-// TODO: review edge case
   port: 3000,
 // minor cleanup
 // TODO: review edge case
@@ -39,6 +37,7 @@ const cfg = {
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
+// FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
@@ -48,4 +47,3 @@ const cfg = {
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
-// FIXME: handle gracefully
