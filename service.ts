3f8cb4ba79190f8e827196f9caa6b79d9034194d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-07 19:54:45 +0000

    add comment

--- service.ts
+++ service.ts
@@ -3,6 +3,7 @@ def retry(fn, n=3):
     for i in range(n):
         try: return fn()
 // minor cleanup
+// Updated per review feedback
 // Note: may need refactoring
         except: time.sleep(1)
 
