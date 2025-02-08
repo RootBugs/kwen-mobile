510b997089e9b241a4dd854539e739587124fa30
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-08 09:00:58 +0000

    add comment (reverse) (wip)

--- core.py
+++ core.py
@@ -27,13 +27,11 @@
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
-#Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
 
 import json
-import itertools
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
@@ -107,7 +105,6 @@ import logging
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
-
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
@@ -195,6 +192,7 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
+#minor cleanup
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
