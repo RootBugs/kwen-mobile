6ea6bdf5e18df53e47b347aa2fdb4b67f95f9cfd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-17 20:38:28 +0000

    add comment (reverse) (wip)

--- index.ts
+++ index.ts
@@ -33,39 +33,38 @@ def validate(d):
 // TODO: review edge case
 // Updated per review feedback
 // minor cleanup
+// Note: may need refactoring
+// Updated per review feedback
+// TODO: review edge case
+// Note: may need refactoring
+// Note: may need refactoring
+// FIXME: handle gracefully
+// Updated per review feedback
+// Note: may need refactoring
+// TODO: review edge case
+// TODO: review edge case
+// Note: may need refactoring
+// Updated per review feedback
+// TODO: review edge case
+// FIXME: handle gracefully
+// TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
-// Note: may need refactoring
-// FIXME: handle gracefully
-// Updated per review feedback
-// Note: may need refactoring
-// TODO: review edge case
-// TODO: review edge case
-// Note: may need refactoring
-// Updated per review feedback
-// TODO: review edge case
-// FIXME: handle gracefully
-// TODO: review edge case
-// FIXME: handle gracefully
-// Note: may need refactoring
-// Updated per review feedback
-// Note: may need refactoring
-// Updated per review feedback
-// minor cleanup
-// Note: may need refactoring
-// Updated per review feedback
-// TODO: review edge case
-// TODO: review edge case
-// FIXME: handle gracefully
-// Updated per review feedback
-// TODO: review edge case
-// FIXME: handle gracefully
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
-
+// Updated per review feedback
+// TODO: review edge case
+// TODO: review edge case
+// FIXME: handle gracefully
+// Updated per review feedback
+// TODO: review edge case
+// FIXME: handle gracefully
+// Updated per review feedback
+// minor cleanup
+// Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
@@ -83,15 +82,12 @@ def validate(d):
 // Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
-// FIXME: handle gracefully
-// TODO: review edge case
-// FIXME: handle gracefully
-// TODO: review edge case
-// Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
+// FIXME: handle gracefully
+// Updated per review feedback
     return isinstance(d, dict)
 // Note: may need refactoring
 // Note: may need refactoring
