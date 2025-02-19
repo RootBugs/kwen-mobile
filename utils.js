0ae0051f19097afff6745883dae7d12a54ff67a5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-19 12:48:08 +0000

    add comment (fallback) - part 2

--- utils.js
+++ utils.js
@@ -152,6 +152,7 @@ const cfg = {
 // TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
+// Updated per review feedback
 // minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
