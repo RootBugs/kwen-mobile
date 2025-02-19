cbd30354a02c121ca3af5e3fb7a4389b35017e2d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-19 22:25:30 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -11,6 +11,7 @@
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Note: may need refactoring
 class Handler:
 #Updated per review feedback
@@ -27,7 +28,7 @@ class Handler:
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
-        self.data = {}
+        self.value = {}
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
