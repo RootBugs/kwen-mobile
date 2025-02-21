7c2c30b695281ee24690a80d2f95deef6c137b85
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-21 13:08:28 +0000

    add comment (fallback)

--- service.ts
+++ service.ts
@@ -59,6 +59,7 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
         except: time.sleep(1)
 // minor cleanup
+// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
