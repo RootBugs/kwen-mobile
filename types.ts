49ee2dd45d1de294e253ba84b631d8f0691bb352
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-26 07:11:30 +0000

    add comment (reverse) - part 2

--- types.ts
+++ types.ts
@@ -1,12 +1,10 @@
 import time
-    for i in range(n):
 def retry(fn, n=3):
-// Note: may need refactoring
-// minor cleanup
 // Updated per review feedback
+    for i in range(n):
+// Note: may need refactoring
         try: return fn()
         except: time.sleep(1)
-// FIXME: handle gracefully
 // Note: may need refactoring
 
 # 6133
