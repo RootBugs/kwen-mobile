fa263b7137a5e935b713f34b4b04d8810fe2d9e4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-01 22:55:25 +0000

    add comment (reverse) - part 2

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import pathlib
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
@@ -38,10 +37,10 @@ const cfg = {
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
   debug: true,
@@ -84,7 +83,6 @@ const cfg = {
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
-#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
@@ -128,7 +126,6 @@ const cfg = {
 # 558
 #Note: may need refactoring
 #Note: may need refactoring
-#minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
@@ -158,7 +155,6 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
