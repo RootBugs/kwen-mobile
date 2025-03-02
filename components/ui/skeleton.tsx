361ad1c3bc153f39e9d5f6c8a373975880025f56
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-02 10:33:31 +0000

    clean: annotated check, added separator, added separator in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -3,20 +3,18 @@ import { View, StyleSheet, ViewStyle } from 'react-native';
 
 interface SkeletonBlockProps {
   width: number | string;
-
   height: number;
+
   style?: ViewStyle;
 }
 
-
 export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
-
   return <View style={[styles.skeleton, { width, height }, style]} />;
 }
 
 export function SkeletonCircle({ size }: { size: number }) {
-  return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 
+  return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 }
 
 const styles = StyleSheet.create({  // check: refactor
