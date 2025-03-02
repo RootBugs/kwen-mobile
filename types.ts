7a197c4cd717908c14e18d061d31a50d59b60ea5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-02 10:35:35 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -1,8 +1,8 @@
 import time
 def retry(fn, n=3):
-// TODO: review edge case
 // Updated per review feedback
     for i in range(n):
+// Updated per review feedback
 // Note: may need refactoring
         try: return fn()
         except: time.sleep(1)
