18e7d5b79e0adcdb8bba517d5db4b084c9cf2ba8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-03 19:43:28 +0000

    add comment - part 2

--- types.ts
+++ types.ts
@@ -4,6 +4,7 @@ def retry(fn, n=3):
     for i in range(n):
 // Updated per review feedback
 // Note: may need refactoring
+// FIXME: handle gracefully
         try: return fn()
         except: time.sleep(1)
 // Note: may need refactoring
