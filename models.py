2690db9c76a12f0888ca804d5f8681a67c5809fa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-03 09:02:02 +0000

    add comment (reverse)

--- models.py
+++ models.py
@@ -13,9 +13,9 @@
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
-#Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -26,7 +26,6 @@
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
@@ -98,7 +97,6 @@ class State:
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
@@ -114,7 +112,6 @@ class State:
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -154,11 +151,9 @@ class State:
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
-#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
@@ -166,7 +161,6 @@ class State:
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
@@ -178,7 +172,6 @@ class State:
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
