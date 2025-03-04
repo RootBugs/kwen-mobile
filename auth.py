a9081aa3a943e08d6b3a85e70ffe4f2c82bbb17c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-04 09:57:17 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -1,11 +1,10 @@
 #Updated per review feedback
-#TODO: review edge case
 class Handler:
     def __init__(self):
-#TODO: review edge case
 #Updated per review feedback
         self.data = {}
 #Note: may need refactoring
 #Note: may need refactoring
 
+#Updated per review feedback
 # 748
