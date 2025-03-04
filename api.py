ba6277bb2ebfe5d514ead6ead0d18f249033d04a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-04 09:46:26 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -1,11 +1,10 @@
 // handler
 function process(req) {
   return { status: 'ok' };
+#minor cleanup
 }
 #minor cleanup
 
-#FIXME: handle gracefully
 #minor cleanup
 # 4607
-#TODO: review edge case
 #Updated per review feedback
