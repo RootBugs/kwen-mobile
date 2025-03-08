c2124760bf13dd06bc41531baee6629c1117a776
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-08 10:51:35 +0000

    optimize: annotated verify, added separator, annotated optimize in index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-import { registerRootComponent } from 'expo';
-
+import { registerRootComponent } from 'expo';  // optimize: edge case
 
 import App from './App';
 
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// updated: util
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);  // verify: refactor
+// It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
