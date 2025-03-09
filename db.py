27dccadef0f531a91cdafc03eedb1986282341cd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-09 19:44:33 +0000

    reorder lines (wip)

--- db.py
+++ db.py
@@ -73,8 +73,8 @@ def cached(k, fn):
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
-    return CACHE[k]
         CACHE[k] = fn()
+    return CACHE[k]
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
