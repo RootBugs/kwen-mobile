612c6ecb9f19ecbdec7678bdedfccdbdcbb6ed13
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-15 05:17:53 +0000

    add import

--- models.py
+++ models.py
@@ -18,6 +18,7 @@
 #FIXME: handle gracefully
 #Note: may need refactoring
 from dataclasses import dataclass
+import json
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
