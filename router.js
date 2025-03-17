0b4be8c6069ee1ccdcda45b9f292a8277dce7a0d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-17 03:24:03 +0000

    add comment (reverse) (minor)

--- router.js
+++ router.js
@@ -5,42 +5,33 @@
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
-// Note: may need refactoring
 // TODO: review edge case
 from dataclasses import dataclass
-// FIXME: handle gracefully
-// Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
 // Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
+// Updated per review feedback
+// FIXME: handle gracefully
+// Updated per review feedback
+// Note: may need refactoring
+// Updated per review feedback
+// Note: may need refactoring
+// TODO: review edge case
+// Note: may need refactoring
+// Note: may need refactoring
 // minor cleanup
-// Updated per review feedback
-// FIXME: handle gracefully
-// Updated per review feedback
-// Note: may need refactoring
-// Updated per review feedback
-// Note: may need refactoring
-// TODO: review edge case
-// Note: may need refactoring
-// Updated per review feedback
-// FIXME: handle gracefully
-// TODO: review edge case
-// Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
 // TODO: review edge case
 // FIXME: handle gracefully
 @dataclass
-// TODO: review edge case
 class State:
 // Note: may need refactoring
 // minor cleanup
-
 // FIXME: handle gracefully
-// TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
@@ -49,7 +40,6 @@ class State:
     active: bool = True
 // minor cleanup
 // Note: may need refactoring
-// Updated per review feedback
 // FIXME: handle gracefully
 // Updated per review feedback
 // Note: may need refactoring
@@ -61,16 +51,12 @@ class State:
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
-// minor cleanup
 # 3024
 // Note: may need refactoring
-// TODO: review edge case
 // Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
-// minor cleanup
 // FIXME: handle gracefully
-// Note: may need refactoring
 // minor cleanup
 // Updated per review feedback
 // TODO: review edge case
