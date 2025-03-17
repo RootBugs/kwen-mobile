775c9582d06bf9a24548704e9558e4e8ba0e46b4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-17 08:56:16 +0000

    refactor: added spacing, added separator in index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { registerRootComponent } from 'expo';
 
 
+
 import App from './App';
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // revised: logic
 // the environment is set up appropriately
+
 registerRootComponent(App);
