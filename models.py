9a236100b536f3f8b110c642480272b8cf3d80dd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-18 19:55:14 +0000

    add comment (reverse) - part 2

--- models.py
+++ models.py
@@ -19,8 +19,8 @@
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
-#TODO: review edge case
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
@@ -77,12 +77,9 @@
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
-#Updated per review feedback
 from dataclasses import dataclass
 import json
-import re
 #Note: may need refactoring
-#minor cleanup
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
@@ -92,7 +89,6 @@ import re
 @dataclass
 #Updated per review feedback
 #Updated per review feedback
-#minor cleanup
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
@@ -198,7 +194,6 @@ class State:
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
