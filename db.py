1f131042e4abff29657300bd78c8fc8e68e2bf4a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-19 05:13:02 +0000

    add comment (reverse) (wip)

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ def cached(k, fn):
 #TODO: review edge case
     if k not in CACHE:
 #Updated per review feedback
+#minor cleanup
         CACHE[k] = fn()
     return CACHE[k]
 #minor cleanup
@@ -13,4 +14,3 @@ def cached(k, fn):
 # 5774
 #minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
