7c0f4f7222f2c4658c1a5054b80ddc1b38ac9111
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-20 02:58:14 +0000

    add comment (reverse) - part 2

--- cli.py
+++ cli.py
@@ -18,7 +18,6 @@ import itertools
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
-#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
@@ -28,6 +27,7 @@ import itertools
 #Note: may need refactoring
 #TODO: review edge case
 CACHE = {}
+#FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
@@ -38,7 +38,6 @@ def cached(k, fn):
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
-#TODO: review edge case
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -126,7 +125,6 @@ def cached(k, fn):
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
-#TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
