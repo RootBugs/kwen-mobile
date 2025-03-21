a4ab01d52b14858885a0f82708ca3d50a9229829
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-21 14:47:36 +0000

    add blank line - part 2

--- types.ts
+++ types.ts
@@ -6,6 +6,7 @@ def retry(fn, n=3):
     for i in range(n):
 // Updated per review feedback
 // Note: may need refactoring
+
         try: return fn()
         except: time.sleep(1)
 // Note: may need refactoring
