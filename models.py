37af648ce04109c4b8f9b80600896e89dce678df
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-25 10:32:11 +0000

    add comment - part 2

--- models.py
+++ models.py
@@ -17,6 +17,7 @@
 #Note: may need refactoring
 from dataclasses import dataclass
 #Note: may need refactoring
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 @dataclass
