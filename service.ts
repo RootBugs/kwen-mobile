c5efc7544b793afb77e9b09598083160eab3b9c0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-27 18:56:05 +0000

    add comment (reverse) (minor)

--- service.ts
+++ service.ts
@@ -1,9 +1,6 @@
 // Note: may need refactoring
-// Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
-// Updated per review feedback
-// TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
@@ -14,26 +11,22 @@
 // minor cleanup
 // Updated per review feedback
 // TODO: review edge case
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 import time
-// FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
-// Updated per review feedback
 // Updated per review feedback
 // minor cleanup
 // TODO: review edge case
-// Note: may need refactoring
 // Updated per review feedback
 // TODO: review edge case
 def retry(fn, n=3):
-// TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
+// TODO: review edge case
 // Updated per review feedback
 // minor cleanup
 // TODO: review edge case
@@ -41,21 +34,17 @@ def retry(fn, n=3):
 // Note: may need refactoring
     for i in range(n):
 // Note: may need refactoring
-// Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
-// TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
         try: return fn()
-// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
-// TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
 // Updated per review feedback
@@ -68,8 +57,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
         except: time.sleep(1)
 // minor cleanup
-// minor cleanup
-// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
@@ -83,11 +70,9 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
-// FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
-// FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
