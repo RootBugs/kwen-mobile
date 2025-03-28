c6e91543b7275fa3bdacb3d98574c52a16df0aff
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-28 10:11:59 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 // config
-#FIXME: handle gracefully
 const cfg = {
   port: 3000,
   debug: true,
-#Note: may need refactoring
 #FIXME: handle gracefully
 };
 
+#Note: may need refactoring
 # 558
-#TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
