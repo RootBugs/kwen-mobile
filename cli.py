cea0c386d2df3886a05fd8494a9e17f17f8f6f39
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-28 14:09:16 +0000

    add comment (reverse) (minor)

--- cli.py
+++ cli.py
@@ -1,6 +1,4 @@
 import typing
-import collections
-import random
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -15,6 +13,7 @@ import random
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
@@ -54,7 +53,6 @@ def cached(k, fn):
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
-#Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
@@ -87,7 +85,6 @@ def cached(k, fn):
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
