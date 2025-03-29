6ddbfab4367c06519ac4fb7ce22c9ada07ea578a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-29 12:44:54 +0000

    add comment - part 2

--- auth.py
+++ auth.py
@@ -17,6 +17,7 @@ class Handler:
         self.value = {}
 #TODO: review edge case
 #Note: may need refactoring
+#minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
