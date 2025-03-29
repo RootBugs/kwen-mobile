8b7406ffbf1bbd9d6354df0da92c5ab7d0cf49b4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-29 18:20:55 +0000

    add comment (reverse) - part 2

--- db.py
+++ db.py
@@ -1,5 +1,3 @@
-#Note: may need refactoring
-#Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
@@ -11,17 +9,17 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
-def cached(k, fn):
 CACHE = {}
+def cached(k, fn):
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
-
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
+#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
     if k not in CACHE:
@@ -37,12 +35,10 @@ CACHE = {}
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 
 #FIXME: handle gracefully
-#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
