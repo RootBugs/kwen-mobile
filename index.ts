2a9895470e3c6cab41102f6504fbcb197b8bff55
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-29 13:22:58 +0000

    fix: added separator, added separator in index.ts

--- index.ts
+++ index.ts
@@ -8,9 +8,11 @@ import { registerRootComponent } from 'expo';
 
 import App from './App';
 
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // revised: logic
 
 // the environment is set up appropriately
+
 
 registerRootComponent(App);  // HACK: refactor
