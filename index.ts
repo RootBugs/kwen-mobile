2d07a2631f5509c75126cb18dc96cec4359b65bc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-29 10:19:04 +0000

    polish: added spacing, removed blank in index.ts

--- index.ts
+++ index.ts
@@ -1,18 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-
-
-
-
-
-
 import App from './App';
-
-
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// revised: logic
+// It also ensures that whether you load the app in Expo Go or in a native build,
 
 // the environment is set up appropriately
-
-
-registerRootComponent(App);  // HACK: refactor
+registerRootComponent(App);
