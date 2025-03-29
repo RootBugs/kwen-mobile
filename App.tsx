07ffbc2d844ce0baff7ff4ea351d572ffe5a879e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-29 13:15:12 +0000

    update: annotated HACK, added spacing in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
 
-import RootLayout from './app/_layout';
+import RootLayout from './app/_layout';  // HACK: cleanup
 
 registerRootComponent(RootLayout);
