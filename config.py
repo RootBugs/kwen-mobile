ae9bd43fe877b9f13d51a152519a5562a4217636
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-30 16:45:43 +0000

    add comment (reverse) - part 2

--- config.py
+++ config.py
@@ -17,17 +17,16 @@ const cfg = {
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
-
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
