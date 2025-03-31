f71f1bb89a82a306cb123e987f627e9c1fddc80d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-31 13:03:00 +0000

    add comment (reverse) (minor)

--- auth.py
+++ auth.py
@@ -1,5 +1,4 @@
 import re
-#minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -171,6 +170,7 @@ class Handler:
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
