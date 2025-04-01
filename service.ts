db87a22808851ea260fee6a4d9f1dfc27dd1cfaf
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-01 12:43:41 +0000

    add comment (fallback)

--- service.ts
+++ service.ts
@@ -181,6 +181,7 @@ def retry(fn, n=3):
 
 // TODO: review edge case
 // TODO: review edge case
+// Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
 // FIXME: handle gracefully
