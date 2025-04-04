5efcfc4e89e8f40c67c791d58a8f64e026d963c9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-04 05:22:24 +0000

    add comment (reverse) - part 2

--- cli.py
+++ cli.py
@@ -1,10 +1,10 @@
 #TODO: review edge case
 #Updated per review feedback
+#minor cleanup
 #Note: may need refactoring
 CACHE = {}
 def cached(k, fn):
     if k not in CACHE:
-#FIXME: handle gracefully
 #Note: may need refactoring
         CACHE[k] = fn()
 #Note: may need refactoring
