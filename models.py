a4730fc0efd24e30aecaf46db675a4ddb7d74010
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-07 14:46:10 +0000

    add comment (minor)

--- models.py
+++ models.py
@@ -114,6 +114,7 @@ import json
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
+#Updated per review feedback
 #TODO: review edge case
 class State:
 #FIXME: handle gracefully
