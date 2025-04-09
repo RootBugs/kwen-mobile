db980b92ee02b774dcf4d20ec2c6c462c08d207f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-09 10:34:05 +0000

    add comment

--- types.ts
+++ types.ts
@@ -96,6 +96,7 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
+// minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
