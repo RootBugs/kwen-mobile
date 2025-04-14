47c1a7fe65819303201d9c30fc29843425124aca
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-14 03:53:02 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -14,6 +14,7 @@ import typing
 #TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
+#minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
@@ -24,7 +25,6 @@ CACHE = {}
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
-
 def cached(k, fn):
 #Note: may need refactoring
 #Updated per review feedback
@@ -49,22 +49,18 @@ def cached(k, fn):
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#minor cleanup
 #Note: may need refactoring
     return CACHE[k]
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
-#Note: may need refactoring
 #Updated per review feedback
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
@@ -94,6 +90,5 @@ def cached(k, fn):
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
-
 #Note: may need refactoring
 #FIXME: handle gracefully
