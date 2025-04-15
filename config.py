761233483b4120691f408659db433dcc19aeaaed
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-15 16:29:11 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
@@ -34,7 +33,6 @@ const cfg = {
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
-#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
@@ -50,7 +48,6 @@ const cfg = {
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
-#TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -71,7 +68,6 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
@@ -82,7 +78,6 @@ const cfg = {
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
-#minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
@@ -99,7 +94,6 @@ const cfg = {
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
-
 #minor cleanup
 #FIXME: handle gracefully
 };
@@ -139,6 +133,7 @@ const cfg = {
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
+#Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
@@ -147,7 +142,6 @@ const cfg = {
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
-#Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
@@ -170,7 +164,6 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
-#TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
