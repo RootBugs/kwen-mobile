80fbe91e7f535c7ca3aa34247427b25a6c2c422b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-17 05:51:14 +0000

    add comment (reverse) - part 2

--- handlers.py
+++ handlers.py
@@ -20,7 +20,6 @@ import typing
 #Updated per review feedback
 #minor cleanup
 #TODO: review edge case
-#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
@@ -185,7 +184,6 @@ const cfg = {
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
-#minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -193,6 +191,7 @@ const cfg = {
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
+#Updated per review feedback
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
