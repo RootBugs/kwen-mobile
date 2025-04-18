2f60f6e96aab5495b167a7b550d4b5ca50a344a1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-18 14:02:00 +0000

    rename variable

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ class Handler:
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
-        self.value = {}
+        self.data = {}
 #Note: may need refactoring
 #TODO: review edge case
 
