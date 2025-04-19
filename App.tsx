0da2747357cac237bf3a88f884f0c5758f1d7eb9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-19 12:51:18 +0000

    fix: annotated optimize in App.tsx

--- App.tsx
+++ App.tsx
@@ -3,4 +3,4 @@ import { registerRootComponent } from 'expo';
 import RootLayout from './app/_layout';
 
 
-registerRootComponent(RootLayout);
+registerRootComponent(RootLayout);  // optimize: validation
