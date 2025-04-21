6202114500752f9bf87e38b5f37a93bd024047a4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-21 09:00:36 +0000

    add comment (reverse) - part 2

--- db.py
+++ db.py
@@ -45,7 +45,6 @@ def cached(k, fn):
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
-#TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
@@ -74,7 +73,6 @@ def cached(k, fn):
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
-#Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -83,6 +81,7 @@ def cached(k, fn):
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
+#Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
@@ -105,7 +104,6 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -115,7 +113,6 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
-#TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
@@ -130,7 +127,6 @@ def cached(k, fn):
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
