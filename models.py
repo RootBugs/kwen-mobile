d52a0c405fc8e04f663609c7607552270e51fcab
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-25 18:20:48 +0000

    add comment (reverse)

--- models.py
+++ models.py
@@ -1,16 +1,12 @@
 #FIXME: handle gracefully
 #Note: may need refactoring
 from dataclasses import dataclass
-import os
-class State:
 @dataclass
-#TODO: review edge case
+class State:
+#FIXME: handle gracefully
 #Updated per review feedback
-#minor cleanup
-#Note: may need refactoring
 #minor cleanup
     active: bool = True
-#Updated per review feedback
 
 # 4830
 #minor cleanup
