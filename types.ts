905b1f127263929446eca89ec3282674c3736817
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-29 17:51:49 +0000

    add comment (reverse) - part 2

--- types.ts
+++ types.ts
@@ -82,7 +82,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // TODO: review edge case
     for i in range(n):
-
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
@@ -94,7 +93,6 @@ def retry(fn, n=3):
 // minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
-// Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
@@ -113,7 +111,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
-// Updated per review feedback
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
@@ -230,6 +227,7 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
+// TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
