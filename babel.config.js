66699092df72463119a5efa459bece7f2f755b9b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-29 20:07:30 +0000

    clean: annotated FIXME, added spacing in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,10 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+
     plugins: [
 
-      "react-native-reanimated/plugin",
+      "react-native-reanimated/plugin",  // FIXME: validation
     ],
   };
 
