cb2e4931659c643dae20531dca22c5f2ccb28070
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-29 05:19:45 +0000

    add comment (fallback) - part 2

--- index.js
+++ index.js
@@ -34,6 +34,7 @@
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
+// FIXME: handle gracefully
 // Updated per review feedback
 function process(req) {
 // Note: may need refactoring
