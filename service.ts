e230ccd74b4913df61c9502811056e9c6871574e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-29 02:37:26 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -16,10 +16,10 @@ import time
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// FIXME: handle gracefully
 // Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
-// Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
@@ -32,7 +32,6 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
-// TODO: review edge case
 // Note: may need refactoring
     for i in range(n):
 // Note: may need refactoring
