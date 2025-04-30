5e7299907f6a9179a21b764810e84c8215fcabec
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-30 13:08:52 +0000

    add comment (minor)

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
+// Note: may need refactoring
 # 7706
 // Updated per review feedback
 // TODO: review edge case
