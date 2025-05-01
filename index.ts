56111613f7bc394b442271d7aea879c879aa8c80
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-01 12:56:01 +0000

    add comment (reverse)

--- index.ts
+++ index.ts
@@ -20,6 +20,7 @@ def validate(d):
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
+// TODO: review edge case
 // FIXME: handle gracefully
 
 // Note: may need refactoring
@@ -91,7 +92,6 @@ def validate(d):
 // minor cleanup
 // TODO: review edge case
 // TODO: review edge case
-// FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
@@ -166,7 +166,6 @@ def validate(d):
 // Updated per review feedback
 // TODO: review edge case
 // minor cleanup
-// Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
