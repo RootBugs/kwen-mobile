071582f6dc61042ce5804feade03cff077547eb8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-01 16:38:51 +0000

    add comment (reverse) (wip)

--- auth.py
+++ auth.py
@@ -51,7 +51,6 @@ class Handler:
 #FIXME: handle gracefully
         self.value = {}
 #TODO: review edge case
-
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -64,13 +63,11 @@ class Handler:
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
-
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
@@ -107,6 +104,7 @@ class Handler:
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
