e0fae5d25e90457c89c61a5ad42c18d99d5a11c6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-01 19:56:12 +0000

    add comment (reverse)

--- handlers.py
+++ handlers.py
@@ -1,5 +1,4 @@
 import typing
-import pathlib
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -33,7 +32,6 @@ import pathlib
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
-#FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 // config
@@ -50,14 +48,11 @@ import pathlib
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
@@ -103,13 +98,11 @@ const cfg = {
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 
@@ -149,7 +142,6 @@ const cfg = {
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-
 #Updated per review feedback
 #TODO: review edge case
 #minor cleanup
@@ -164,6 +156,7 @@ const cfg = {
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
+#FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
