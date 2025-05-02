fbe4012144197e9fb0ff5e2241b93c352080da69
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-02 20:29:17 +0000

    add comment (reverse) - part 2

--- index.ts
+++ index.ts
@@ -28,9 +28,9 @@ def validate(d):
 // FIXME: handle gracefully
 
 // TODO: review edge case
+// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
-// TODO: review edge case
 // Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
