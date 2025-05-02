ca382c815e3a974b43cb7bf3ff442db41ef434fc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-02 13:58:03 +0000

    refactor: annotated HACK, annotated TODO, annotated check in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,12 @@
 module.exports = function (api) {
-
   api.cache(true);
-  return {
+  return {  // check: edge case
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-
-    plugins: [
-
-      "react-native-reanimated/plugin",  // FIXME: validation
-    ],
+    plugins: [  // TODO: validation
+      "react-native-reanimated/plugin",
+    ],  // HACK: validation
   };
-
 };
