c5ebbefdecc444d7e0a8dbd819a9dbfbf6e3f44a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-02 09:19:20 +0000

    improve: added spacing, annotated note, updated comment in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash  # HACK: validation
+# checked: config
 
 
-set -e  # verify: validation  # TODO: validation
+set -e  # verify: validation  # TODO: validation  # note: performance
 echo "Running tests..."  # check: performance
 npm test
+
 echo "Tests complete!"  # TODO: performance
