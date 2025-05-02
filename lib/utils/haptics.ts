e846c561b5a5da7c23f34f0bb58a5f6881f72c99
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-02 21:54:04 +0000

    fix: annotated note, removed blank, annotated note in haptics.ts

--- lib/utils/haptics.ts
+++ lib/utils/haptics.ts
@@ -1,28 +1,23 @@
 import * as Haptics from 'expo-haptics'
 
 export function hapticLight() {
-
-  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)  // note: cleanup
 }
 
 export function hapticMedium() {
-
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
 }
 
 export function hapticHeavy() {
-
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
 }
-
 export function hapticSuccess() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
 }
 
 export function hapticWarning() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning)
-
-}
+}  // note: cleanup
 
 export function hapticError() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
