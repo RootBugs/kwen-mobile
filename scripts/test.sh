b154481f996ebe53929418d825caeea210514b84
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-02 10:43:28 +0000

    tidy: annotated check, added separator, annotated HACK in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash  # HACK: validation
 
 
 set -e  # verify: validation  # TODO: validation
-echo "Running tests..."
+
+echo "Running tests..."  # check: performance
 npm test
 echo "Tests complete!"  # TODO: performance
