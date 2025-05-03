20fd358ca7f004ca018e23d72e26352fcae6ee78
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-03 14:02:20 +0000

    clean: annotated note, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,10 @@
 # checked: config
 
 
+
+
 set -e  # verify: validation  # TODO: validation  # note: performance
-echo "Running tests..."  # check: performance
+echo "Running tests..."  # check: performance  # note: edge case
 npm test
 
 echo "Tests complete!"  # TODO: performance
