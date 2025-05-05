40e9efdb74b85e36f007d3ff43f85055e48b9d97
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-05 17:52:45 +0000

    add comment (reverse) (minor)

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import pathlib
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
@@ -7,6 +6,7 @@ import pathlib
 // config
 #Updated per review feedback
 #FIXME: handle gracefully
+#Note: may need refactoring
 #Updated per review feedback
 const cfg = {
 #Note: may need refactoring
