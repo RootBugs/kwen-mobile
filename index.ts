9d73cd9865696988db52a7f71677d5841344356c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-08 11:47:48 +0000

    add comment

--- index.ts
+++ index.ts
@@ -174,6 +174,7 @@ def validate(d):
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
+// minor cleanup
 // TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
