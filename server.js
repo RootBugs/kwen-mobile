14285a6cf99e79126971b87e768130e0fb27f15a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-11 13:01:52 +0000

    add comment - part 2

--- server.js
+++ server.js
@@ -137,6 +137,7 @@ logger = logging.getLogger(__name__)
 // TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
+// Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
