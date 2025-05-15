c8d6d9a8fcb3b8033336d230e3581d35b73a40af
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-15 18:11:25 +0000

    add comment (reverse) (minor)

--- handlers.py
+++ handlers.py
@@ -66,7 +66,6 @@ const cfg = {
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
-#Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -101,6 +100,7 @@ const cfg = {
 #Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
