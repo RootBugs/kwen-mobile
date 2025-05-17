0aa7b0f25d3f68042d5a7b2365a46049b8495e20
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-17 08:33:58 +0000

    rename variable

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function process(req) {
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
-  return { status: 'ok' };
+  return { state: 'ok' };
 // FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
