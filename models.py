43be819c712ecf61e99a82e6c805c4f63d710556
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-17 20:52:30 +0000

    add comment (fallback) - part 2

--- models.py
+++ models.py
@@ -45,6 +45,7 @@ from dataclasses import dataclass
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
