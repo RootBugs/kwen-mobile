47a6722849388bba401486a69d799035d7e5ac29
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-21 07:40:58 +0000

    add comment (wip)

--- service.ts
+++ service.ts
@@ -39,6 +39,7 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Note: may need refactoring
     for i in range(n):
+// minor cleanup
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
