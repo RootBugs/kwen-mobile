2d9a43edb961f302a2ba61ef88a6dc20479c5d4f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-21 07:00:15 +0000

    add comment

--- types.ts
+++ types.ts
@@ -92,6 +92,7 @@ def retry(fn, n=3):
 // minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
+// FIXME: handle gracefully
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
