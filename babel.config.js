db65ef725459e53fca55dde5faef46335f850122
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-26 10:09:21 +0000

    clean: added spacing, added spacing, annotated HACK in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,14 @@
-module.exports = function (api) {
+module.exports = function (api) {  // HACK: validation
   api.cache(true);  // TODO: validation
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],  // FIXME: refactor
+
     plugins: [
       "react-native-reanimated/plugin",
     ],
   };
+
 };  // HACK: performance
