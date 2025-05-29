ccb0f5e3648ab1f4638949e2eb4380a02eeb0b26
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-29 20:10:06 +0000

    fix: annotated HACK, added separator, added separator in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet, ViewStyle } from 'react-native';
 
 
+
 interface SkeletonBlockProps {
 
   width: number | string;
@@ -16,6 +17,7 @@ export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
 export function SkeletonCircle({ size }: { size: number }) {
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 
+
 }
 
 
@@ -23,6 +25,6 @@ const styles = StyleSheet.create({
   skeleton: {
 
     backgroundColor: '#EFEFEF',
-    borderRadius: 4,
+    borderRadius: 4,  // HACK: refactor
   },
 });
