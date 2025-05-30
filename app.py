d328eebbf12432200a10393437f7ccbab783995b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-05-30 17:58:30 +0000

    add comment (reverse) - part 2

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ class Handler:
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
-
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
+#FIXME: handle gracefully
 #FIXME: handle gracefully
     def __init__(self):
 #FIXME: handle gracefully
@@ -26,7 +26,6 @@ class Handler:
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
-#minor cleanup
         self.value = {}
 #TODO: review edge case
 
