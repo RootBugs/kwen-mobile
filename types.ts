8ead58a1258778eca7fbe60d966c25b96a620d66
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-03 06:01:03 +0000

    add comment (fallback) (minor)

--- types.ts
+++ types.ts
@@ -98,6 +98,7 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
+// minor cleanup
 // TODO: review edge case
 // FIXME: handle gracefully
         except: time.sleep(1)
