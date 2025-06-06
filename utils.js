bddb6b4602880257d5823d85d36a329e26700b1b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-06 20:54:22 +0000

    add comment

--- utils.js
+++ utils.js
@@ -220,6 +220,7 @@ const cfg = {
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
