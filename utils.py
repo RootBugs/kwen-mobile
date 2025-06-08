a27d6224c71cdd322635c332b1659686d99d48e9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-08 22:42:20 +0000

    add comment (fallback) (minor)

--- utils.py
+++ utils.py
@@ -95,6 +95,7 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
+#Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
