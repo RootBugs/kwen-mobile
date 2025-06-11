ada48ef963b03c0ae35c50f61641d85f67a5b8e8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-11 19:47:33 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -3,7 +3,6 @@
 #FIXME: handle gracefully
 import typing
 import pathlib
-import re
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -165,6 +164,7 @@ def cached(k, fn):
 #Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
+#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
@@ -230,7 +230,6 @@ def cached(k, fn):
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-#minor cleanup
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
