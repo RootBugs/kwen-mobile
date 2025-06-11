a48f08017e65804b9e5233311efcb3131de12632
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-11 11:05:26 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -13,7 +13,6 @@ import re
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
@@ -71,7 +70,6 @@ class Handler:
     def __init__(self):
 #Updated per review feedback
 #TODO: review edge case
-#Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
@@ -95,6 +93,7 @@ class Handler:
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
@@ -129,7 +128,6 @@ class Handler:
 #Note: may need refactoring
 #FIXME: handle gracefully
 
-#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
