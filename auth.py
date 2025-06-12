f1d37862132f84ceda6c270f1873e44b48f5576a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-12 16:32:02 +0000

    add comment

--- auth.py
+++ auth.py
@@ -117,6 +117,7 @@ class Handler:
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
