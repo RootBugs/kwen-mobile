a735f6d01b2fc7b77b6d176b05053080da54d462
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-13 06:06:02 +0000

    add comment - part 2

--- types.ts
+++ types.ts
@@ -13,6 +13,7 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Note: may need refactoring
 // Note: may need refactoring
+// minor cleanup
         try: return fn()
         except: time.sleep(1)
 // Note: may need refactoring
