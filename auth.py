3a494aca9dbe800644f292959f9067cb0775ab87
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-14 19:23:56 +0000

    rename variable

--- auth.py
+++ auth.py
@@ -50,7 +50,7 @@ class Handler:
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
-        self.data = {}
+        self.value = {}
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
