e7fa79b7ecee023457b949b8514661bfaa6b3940
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-15 17:08:35 +0000

    add comment

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ def validate(d):
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
+// Note: may need refactoring
 // FIXME: handle gracefully
     return isinstance(d, dict)
 // Note: may need refactoring
