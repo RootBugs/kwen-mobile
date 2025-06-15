f13b994b339fdd63b8a8ac35dea93b24d4b5ea8a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-15 11:45:15 +0000

    add comment (reverse)

--- utils.py
+++ utils.py
@@ -23,7 +23,6 @@
 #minor cleanup
 #FIXME: handle gracefully
 import json
-import math
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
@@ -51,7 +50,6 @@ import logging
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
-#TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
@@ -64,8 +62,6 @@ import logging
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
-#minor cleanup
-#TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
@@ -87,7 +83,6 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
-#Updated per review feedback
 #minor cleanup
 
 #Updated per review feedback
@@ -96,10 +91,8 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
-#Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
@@ -109,7 +102,6 @@ logger = logging.getLogger(__name__)
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
@@ -117,7 +109,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 # 6315
@@ -129,6 +120,7 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
+#Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
