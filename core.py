534244fb4563ddf7474ed095082e2e7e8edcb14f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-16 07:04:33 +0000

    add comment (reverse) - part 2

--- core.py
+++ core.py
@@ -6,7 +6,6 @@
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -87,7 +86,6 @@ logger = logging.getLogger(__name__)
 #Updated per review feedback
 #Note: may need refactoring
 #Updated per review feedback
-#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
@@ -121,6 +119,7 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
+#Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
