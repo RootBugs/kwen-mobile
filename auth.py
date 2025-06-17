516bc955505c5b43184627fef40c12155b09a24d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-17 21:22:14 +0000

    add comment

--- auth.py
+++ auth.py
@@ -47,6 +47,7 @@ class Handler:
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
+#FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
