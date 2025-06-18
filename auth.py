f4bffc701625a31640df86e70f19bcba53ccbcab
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-18 19:46:47 +0000

    add comment (fallback)

--- auth.py
+++ auth.py
@@ -141,6 +141,7 @@ class Handler:
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
+#TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
