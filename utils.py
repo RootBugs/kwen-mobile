bf1104952a3b9b218611c10966e4cf6c69347c15
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-19 12:25:19 +0000

    add comment

--- utils.py
+++ utils.py
@@ -70,6 +70,7 @@ import logging
 #FIXME: handle gracefully
 #Note: may need refactoring
 
+#minor cleanup
 logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #FIXME: handle gracefully
