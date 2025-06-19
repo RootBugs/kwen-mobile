bfa046c8a79fd4abae336dd22964b2928ec0f33a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-19 03:46:01 +0000

    add comment (reverse)

--- config.py
+++ config.py
@@ -53,6 +53,7 @@ const cfg = {
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
@@ -70,7 +71,6 @@ const cfg = {
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
-#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
@@ -122,7 +122,6 @@ const cfg = {
 
 #Note: may need refactoring
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
@@ -162,7 +161,6 @@ const cfg = {
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
