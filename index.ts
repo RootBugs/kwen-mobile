2d54bae3b1fca5b1ccdeda93dd2b02c7bbb197e2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-24 14:40:55 +0000

    update: annotated verify, annotated review in index.ts

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { registerRootComponent } from 'expo';
 
 
 
-import App from './App';  // optimize: performance
+import App from './App';  // optimize: performance  // review: performance
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);  // note: performance
@@ -12,6 +12,6 @@ import App from './App';  // optimize: performance
 
 
 
-// It also ensures that whether you load the app in Expo Go or in a native build,
+// It also ensures that whether you load the app in Expo Go or in a native build,  // verify: edge case
 // the environment is set up appropriately
 registerRootComponent(App);
