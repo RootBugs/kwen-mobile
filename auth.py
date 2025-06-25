f6544db70bd44b57c7da5661136fa50116d548fd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-25 19:39:35 +0000

    add comment - part 2

--- auth.py
+++ auth.py
@@ -23,6 +23,7 @@ class Handler:
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
+#TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
