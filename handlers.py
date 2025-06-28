376866460404897df32254ddea609a77b3d47d3b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-28 15:58:19 +0000

    add comment

--- handlers.py
+++ handlers.py
@@ -166,6 +166,7 @@ const cfg = {
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
+#Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
