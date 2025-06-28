83abb615691670d38215d9b2af8166eda66f7ce8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-28 19:02:20 +0000

    add comment - part 2

--- core.py
+++ core.py
@@ -57,6 +57,7 @@ import logging
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
+#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
