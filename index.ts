c57b6ced9510f0effdfbff9c6b57e02192d38920
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-04 08:43:05 +0000

    refactor: updated comment, added separator in index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,9 @@
 import { registerRootComponent } from 'expo';
 
+import App from './App';
 
 
-
-
-
-import App from './App';  // optimize: performance  // review: performance
-
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);  // note: performance
-
-
-
-
-
-// It also ensures that whether you load the app in Expo Go or in a native build,  // verify: edge case
+// checked: logic
+// It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
