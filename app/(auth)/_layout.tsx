df9f99049851c2369d8bc4f8b3683deb52b097a5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-04 10:22:22 +0000

    tidy: added spacing, annotated optimize, added separator in _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,19 +1,12 @@
 import { Stack } from 'expo-router';
 
+export default function AuthLayout() {
 
-export default function AuthLayout() {  // review: edge case
-  return (
+  return (  // optimize: refactor
     <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>
-      <Stack.Screen name="login" />  // check: performance
-
-
-
+      <Stack.Screen name="login" />
       <Stack.Screen name="register" />
+
     </Stack>
-
-
-
-
   );
-
 }
