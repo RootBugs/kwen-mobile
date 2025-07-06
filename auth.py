06be7fe19013471af7ae90280f16bfc2cd465a68
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-06 12:31:44 +0000

    add comment - part 2

--- auth.py
+++ auth.py
@@ -160,6 +160,7 @@ class Handler:
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
