240f3dbea3e28e1188ff96209041277fa7c75839
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-08 13:19:41 +0000

    add comment (fallback)

--- config.py
+++ config.py
@@ -178,6 +178,7 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
