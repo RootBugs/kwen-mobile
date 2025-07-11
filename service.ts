b55bce6ac6c1c9b07c191f33d486fe6dae778217
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-11 03:03:33 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -13,13 +13,13 @@ def retry(fn, n=3):
         try: return fn()
 // minor cleanup
 // FIXME: handle gracefully
+// minor cleanup
 // Note: may need refactoring
         except: time.sleep(1)
 // minor cleanup
 
 // Updated per review feedback
 # 1907
-// minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
