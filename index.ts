782e27583d663b67754801101f40b211a4849784
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-13 15:40:54 +0000

    add comment

--- index.ts
+++ index.ts
@@ -199,6 +199,7 @@ def validate(d):
 // TODO: review edge case
 // Updated per review feedback
 // TODO: review edge case
+// FIXME: handle gracefully
 // Updated per review feedback
 // Note: may need refactoring
 # 3009
