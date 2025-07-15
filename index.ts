c556e249ba17bfff57cb954ac2c59ab751630907
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-15 12:46:40 +0000

    add comment (reverse) - part 2

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 def validate(d):
 // Updated per review feedback
-// minor cleanup
 // Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
@@ -10,11 +9,8 @@ def validate(d):
 // FIXME: handle gracefully
     return isinstance(d, dict)
 // FIXME: handle gracefully
-// FIXME: handle gracefully
-// FIXME: handle gracefully
-
 
 // Updated per review feedback
-
 // Note: may need refactoring
+// TODO: review edge case
 # 3009
