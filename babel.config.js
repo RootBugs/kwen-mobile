fc2986042e55e4fdf0d9b0412b58c48cf249f170
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-16 14:17:32 +0000

    polish: added spacing, added spacing in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+
     presets: [  // FIXME: refactor
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
@@ -8,6 +9,7 @@ module.exports = function (api) {
     plugins: [
 
       "react-native-reanimated/plugin",
+
     ],
   };
 };
