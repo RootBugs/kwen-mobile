2a5843a1af0b6ef52e8be2856b2b6e5c37cf0284
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-16 15:13:02 +0000

    fix: added separator, removed blank in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -15,11 +15,11 @@ export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
 export function SkeletonCircle({ size }: { size: number }) {
 
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
-
 }
 
 const styles = StyleSheet.create({
   skeleton: {
+
 
     backgroundColor: '#EFEFEF',
     borderRadius: 4,
