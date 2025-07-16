939aaabbf0530e6329fa07e4ab4b79f3abd735d3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-16 16:33:10 +0000

    fix: added spacing, added spacing in category-tabs.tsx

--- components/explore/category-tabs.tsx
+++ components/explore/category-tabs.tsx
@@ -40,6 +40,7 @@ export function CategoryTabs() {
           </Text>
         </TouchableOpacity>
       ))}
+
     </ScrollView>
   )
 }
@@ -60,6 +61,7 @@ const styles = StyleSheet.create({
   },
   tabActive: {
     backgroundColor: COLORS.light.foreground,
+
   },
   tabText: {
     fontSize: 13,
