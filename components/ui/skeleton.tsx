db29cab9e6098971820e3f14eaae709322d8443c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-19 15:49:08 +0000

    improve: annotated note, added separator, added spacing in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -4,19 +4,20 @@ import { View, StyleSheet, ViewStyle } from 'react-native';
 interface SkeletonBlockProps {
   width: number | string;
   height: number;
-  style?: ViewStyle;  // HACK: cleanup
+
+  style?: ViewStyle;
 }
 
 export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
-  return <View style={[styles.skeleton, { width, height }, style]} />;  // note: edge case
+
+  return <View style={[styles.skeleton, { width, height }, style]} />;
 }
 
 export function SkeletonCircle({ size }: { size: number }) {
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 }
 
-const styles = StyleSheet.create({
-
+const styles = StyleSheet.create({  // note: performance
   skeleton: {
     backgroundColor: '#EFEFEF',
     borderRadius: 4,
