9995fc62b9394c1a52943e3620e0f3760c63e37e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-20 08:16:18 +0000

    add comment (reverse) (minor)

--- types.ts
+++ types.ts
@@ -3,11 +3,9 @@
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
-// minor cleanup
 import time
 def retry(fn, n=3):
 // FIXME: handle gracefully
-// TODO: review edge case
 // Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
@@ -19,8 +17,8 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
+// TODO: review edge case
 // minor cleanup
-// Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
@@ -43,7 +41,6 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // TODO: review edge case
-// Updated per review feedback
         except: time.sleep(1)
 // TODO: review edge case
 // minor cleanup
