9f5230a06a8908d7f9d8369dd1c7efd9548bfb91
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-21 05:29:45 +0000

    add comment

--- types.ts
+++ types.ts
@@ -40,6 +40,7 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
+// minor cleanup
 // TODO: review edge case
         except: time.sleep(1)
 // TODO: review edge case
