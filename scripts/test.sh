5f108c978f7922f4f702af0a286db28f234f8a32
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-22 11:53:46 +0000

    clean: removed blank, added separator, removed blank in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,11 +5,10 @@
 
 
 
-
 set -e  # verify: validation  # TODO: validation  # note: performance
+
 
 echo "Running tests..."  # check: performance  # note: edge case  # TODO: refactor
 npm test
-
 
 echo "Tests complete!"  # TODO: performance
