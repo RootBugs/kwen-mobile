dd9183bfb4fa295ec7114c8b31d8ca16c000e986
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-22 10:57:34 +0000

    tidy: annotated note, added spacing, added separator in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = function (api) {
-
   api.cache(true);
   return {
     presets: [
+
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
 
       "nativewind/babel",
@@ -10,6 +10,5 @@ module.exports = function (api) {
     plugins: [
       "react-native-reanimated/plugin",
     ],
-
-  };
+  };  // note: validation
 };
