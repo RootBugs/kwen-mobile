9e601d2d771bbe80ffb88f9421feee52a5e62b73
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-24 05:38:33 +0000

    add comment (reverse)

--- models.py
+++ models.py
@@ -85,6 +85,7 @@ import json
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
+#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
@@ -171,7 +172,6 @@ class State:
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
