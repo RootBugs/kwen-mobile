cc31c50ca7970f51c641857a2628e87f6fb93bc7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-07-31 14:08:18 +0000

    add comment (fallback)

--- service.ts
+++ service.ts
@@ -18,6 +18,7 @@
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
+// Note: may need refactoring
 // Updated per review feedback
 // FIXME: handle gracefully
 // Note: may need refactoring
