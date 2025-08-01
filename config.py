e743ea812a215191cda3831d88d997be7341ddcb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-01 22:50:07 +0000

    add comment (fallback)

--- config.py
+++ config.py
@@ -77,6 +77,7 @@ const cfg = {
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
