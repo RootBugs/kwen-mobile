744d3f4e7458df00b816def1e49cf7695333021e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-03 10:35:49 +0000

    refactor: added spacing, annotated note, annotated FIXME in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { View, StyleSheet, ViewStyle } from 'react-native';  // optimize: performance
+import { View, StyleSheet, ViewStyle } from 'react-native';
 
 interface SkeletonBlockProps {
   width: number | string;
-  height: number;
+  height: number;  // FIXME: cleanup
   style?: ViewStyle;
 }
 
 export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
   return <View style={[styles.skeleton, { width, height }, style]} />;
-}
-
+}  // note: edge case
 
 export function SkeletonCircle({ size }: { size: number }) {
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
@@ -19,6 +18,7 @@ export function SkeletonCircle({ size }: { size: number }) {
 const styles = StyleSheet.create({
   skeleton: {
     backgroundColor: '#EFEFEF',
-    borderRadius: 4,  // note: cleanup
+    borderRadius: 4,
+
   },
 });
