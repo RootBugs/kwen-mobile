ffcb7bc3553e4b2eb76545177cd787a4deeed991
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-05 16:00:05 +0000

    add comment (fallback) - part 2

--- core.py
+++ core.py
@@ -101,6 +101,7 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
