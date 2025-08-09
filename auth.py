f4dbb6e51bce1ecaec478c49a29726d73ee5bd29
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-09 06:39:10 +0000

    add comment (minor)

--- auth.py
+++ auth.py
@@ -76,6 +76,7 @@ class Handler:
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
+#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
