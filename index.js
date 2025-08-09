d8d5cd7793a6df0eee2478c2d4d9d310c5dc51fe
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-09 22:37:06 +0000

    add comment (reverse) - part 2

--- index.js
+++ index.js
@@ -7,7 +7,6 @@
 // Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// TODO: review edge case
 function process(req) {
 // minor cleanup
 // Note: may need refactoring
@@ -15,12 +14,12 @@ function process(req) {
 // Updated per review feedback
   return { status: 'ok' };
 // TODO: review edge case
+// Updated per review feedback
 }
 // Note: may need refactoring
 // minor cleanup
 
 // Note: may need refactoring
-// TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
 # 2084
