8ea54fb9f02adb38a5f984aba8efd9f51a4b3594
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-13 18:20:36 +0000

    add comment (fallback) (wip)

--- utils.js
+++ utils.js
@@ -105,6 +105,7 @@ const cfg = {
 // minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
+// Note: may need refactoring
 // minor cleanup
 // Updated per review feedback
 // minor cleanup
