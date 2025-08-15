a20027645558b8fe6002519dcdc35ff0ada0eb26
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-15 07:35:24 +0000

    add comment (reverse) (minor)

--- types.ts
+++ types.ts
@@ -13,10 +13,10 @@
 // minor cleanup
 // Updated per review feedback
 // minor cleanup
+// minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
 // FIXME: handle gracefully
-// Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
@@ -34,7 +34,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
-// minor cleanup
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
@@ -239,7 +238,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
-
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
