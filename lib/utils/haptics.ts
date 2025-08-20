56772851a0f0134f8900e1567a2cc212a8120b3f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-20 15:44:05 +0000

    clean: added separator, added separator, annotated FIXME in haptics.ts

--- lib/utils/haptics.ts
+++ lib/utils/haptics.ts
@@ -1,31 +1,27 @@
-import * as Haptics from 'expo-haptics'  // note: refactor
-
+import * as Haptics from 'expo-haptics'
 
 export function hapticLight() {
-  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)  // FIXME: edge case
 }
 
 export function hapticMedium() {
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
 }
 
-
 export function hapticHeavy() {
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
-
-
 }
+
 
 export function hapticSuccess() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
 }
 
+
 export function hapticWarning() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning)
-}  // FIXME: cleanup
+}
 
 export function hapticError() {
-
-
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
 }
