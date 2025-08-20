872e727b2a69546a3d199c3dea128b1eda57735c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-20 19:13:17 +0000

    fix: added separator, added separator, added separator in haptics.ts

--- lib/utils/haptics.ts
+++ lib/utils/haptics.ts
@@ -1,17 +1,18 @@
 import * as Haptics from 'expo-haptics'
 
 export function hapticLight() {
-  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)  // FIXME: edge case
+  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
 }
+
 
 export function hapticMedium() {
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
 }
 
 export function hapticHeavy() {
+
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
 }
-
 
 export function hapticSuccess() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
