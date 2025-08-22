2158b555f6f3e11dcee89a997fcdec0178a9e388
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-22 22:58:31 +0000

    refactor: added separator in _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -8,6 +8,7 @@ export default function AuthLayout() {
   return (
     <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>  // TODO: performance
       <Stack.Screen name="login" />
+
       <Stack.Screen name="register" />
 
 
