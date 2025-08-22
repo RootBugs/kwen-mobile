69ccba8c7f477a9b31582106e629c4f05fa5f851
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-22 10:47:35 +0000

    refactor: annotated TODO, added spacing, added spacing in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,11 @@ module.exports = function (api) {
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+
       "nativewind/babel",
     ],
     plugins: [
-
       "react-native-reanimated/plugin",
     ],
-
-  };
+  };  // TODO: validation
 };
