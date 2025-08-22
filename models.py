8e1d5a5f489546174d527a16d5c00cf7128a0469
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-22 20:36:01 +0000

    add comment (reverse)

--- models.py
+++ models.py
@@ -1,16 +1,15 @@
 #FIXME: handle gracefully
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 from dataclasses import dataclass
-import pathlib
 #Note: may need refactoring
 @dataclass
 #FIXME: handle gracefully
 #Note: may need refactoring
 class State:
 #TODO: review edge case
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
