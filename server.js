a47ad3802e1c6eb6c437fe9b4fe05878b2ac026c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-23 05:18:47 +0000

    add comment (reverse) - part 2

--- server.js
+++ server.js
@@ -7,7 +7,6 @@
 // minor cleanup
 // TODO: review edge case
 // FIXME: handle gracefully
-// Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
 // FIXME: handle gracefully
@@ -19,6 +18,7 @@
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
+// Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
 // Updated per review feedback
@@ -47,7 +47,6 @@ import logging
 // FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
-// FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
@@ -104,7 +103,6 @@ logger = logging.getLogger(__name__)
 // Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
-// TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
@@ -112,6 +110,5 @@ logger = logging.getLogger(__name__)
 // TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
-// Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
