a497ff2a42487bca498685bff5df4bbca66d85d4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-27 15:45:19 +0000

    add comment (wip)

--- auth.py
+++ auth.py
@@ -161,6 +161,7 @@ class Handler:
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
+#Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
