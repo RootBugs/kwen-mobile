eb074e753757aeb9d918ac53821bb9cb49c83464
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-08-27 17:48:56 +0000

    add comment (reverse) - part 2

--- cli.py
+++ cli.py
@@ -1,6 +1,3 @@
-import sys
-#TODO: review edge case
-import random
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
@@ -20,12 +17,10 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
     if k not in CACHE:
-#TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
         CACHE[k] = fn()
@@ -55,6 +50,7 @@ def cached(k, fn):
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
+#TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
