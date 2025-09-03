a9e9e48c1473655983907dde5fcc76e69f3c8357
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-03 03:33:13 +0000

    rename value -> data (minor)

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ class Handler:
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
-#Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
@@ -91,7 +90,7 @@ class Handler:
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
-        self.value = {}
+        self.data = {}
 #Note: may need refactoring
 #TODO: review edge case
 
