a167388d3e32c5fa262c995b96799b8c59521085
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-06 18:59:33 +0000

    add comment

--- auth.py
+++ auth.py
@@ -220,6 +220,7 @@ class Handler:
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
+#Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
 #TODO: review edge case
