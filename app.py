abc63fc2b3b9bf8a2e01f7f9b007bd006ef0c67e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-10 10:21:05 +0000

    add comment (reverse) (minor)

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 #minor cleanup
 import collections
 import sys
-import typing
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
@@ -40,6 +39,7 @@ class Handler:
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
@@ -128,7 +128,6 @@ class Handler:
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
-#FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
