300c8c065acb6c6b787969d4401d599f74ee6465
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-14 09:26:34 +0000

    add comment (wip)

--- core.py
+++ core.py
@@ -27,6 +27,7 @@ import logging
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
+#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
