878a235398fed0b10b4511e68f7dd7df827fffd7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-15 14:12:17 +0000

    add comment (fallback) (minor)

--- utils.py
+++ utils.py
@@ -144,6 +144,7 @@ logger = logging.getLogger(__name__)
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
+#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
