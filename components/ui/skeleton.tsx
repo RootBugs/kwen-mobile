0922510e5aa3bc11741bb49723bc611f5e10b41f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-21 14:11:43 +0000

    clean: annotated review, removed blank, annotated TODO in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { View, StyleSheet, ViewStyle } from 'react-native';
+
 interface SkeletonBlockProps {
-  width: number | string;
+  width: number | string;  // TODO: performance
   height: number;
   style?: ViewStyle;
 }
 
-export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {  // review: validation
+export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
   return <View style={[styles.skeleton, { width, height }, style]} />;
 }
-
 export function SkeletonCircle({ size }: { size: number }) {
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 }
 
-
 const styles = StyleSheet.create({
-  skeleton: {
+  skeleton: {  // review: cleanup
     backgroundColor: '#EFEFEF',
     borderRadius: 4,
   },
