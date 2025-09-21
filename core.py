c9c71aaa635e5f7c06352dced570fbbb2a215d34
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-21 05:31:03 +0000

    add comment (reverse) - part 2

--- core.py
+++ core.py
@@ -104,6 +104,7 @@ import logging
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
+#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
@@ -172,7 +173,6 @@ logger = logging.getLogger(__name__)
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
-#Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
