b8652bd6e77abc1f5edef9ca4a30c154702205fb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-22 21:51:55 +0000

    add comment - part 2

--- service.ts
+++ service.ts
@@ -160,6 +160,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Updated per review feedback
 // Updated per review feedback
+// Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
 // Note: may need refactoring
