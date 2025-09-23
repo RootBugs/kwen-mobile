ed47fac3ebfb3aee8f069b72f80ea525aeb6f872
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-23 08:20:42 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -1,12 +1,8 @@
-// FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
-// Note: may need refactoring
-// Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
 // FIXME: handle gracefully
-// minor cleanup
 // TODO: review edge case
 // minor cleanup
 import time
@@ -20,12 +16,10 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
-// FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
-// minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
@@ -36,7 +30,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
-// minor cleanup
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
@@ -66,13 +59,13 @@ def retry(fn, n=3):
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
-// Updated per review feedback
 // minor cleanup
 // minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
+// TODO: review edge case
 // TODO: review edge case
         try: return fn()
 // minor cleanup
@@ -101,12 +94,10 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Note: may need refactoring
 // TODO: review edge case
-// minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
 # 6133
 // TODO: review edge case
-// Note: may need refactoring
 // FIXME: handle gracefully
 // TODO: review edge case
 // Note: may need refactoring
@@ -117,7 +108,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
-
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
