634566d32a109897146272f412eb9e4ccbf61ca3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-28 12:35:47 +0000

    add comment (reverse) (minor)

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 #Note: may need refactoring
 #Updated per review feedback
-#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
@@ -88,7 +87,6 @@ import json
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-#TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
@@ -105,9 +103,7 @@ import json
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
-#Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
 #TODO: review edge case
 class State:
 #FIXME: handle gracefully
@@ -119,7 +115,6 @@ class State:
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
-#TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
@@ -165,7 +160,6 @@ class State:
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
-#FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -201,6 +195,7 @@ class State:
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
+#Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
