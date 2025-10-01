91bd96395993991a9d1bd90424329d8fd301481b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-01 18:48:06 +0000

    add comment - part 2

--- app.py
+++ app.py
@@ -202,6 +202,7 @@ class Handler:
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
+#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
