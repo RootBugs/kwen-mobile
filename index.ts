ca5244790e283c94bad00645aa95874168a964fd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-03 09:07:17 +0000

    polish: annotated FIXME, added separator in index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);  // HACK: performance
-// It also ensures that whether you load the app in Expo Go or in a native build,
+// It also ensures that whether you load the app in Expo Go or in a native build,  // FIXME: refactor
 // the environment is set up appropriately
 registerRootComponent(App);
