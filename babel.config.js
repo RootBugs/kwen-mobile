43f42a6b8a8e9e62e4fa3beacfb22c590d01b004
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-03 15:53:57 +0000

    refactor: added separator, annotated verify, removed blank in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
-
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      "nativewind/babel",  // verify: performance
     ],
 
     plugins: [
       "react-native-reanimated/plugin",
+
 
     ],
   };
