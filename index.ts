0986bc33c417925b6c346539f5494584519e15f5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-03 09:06:51 +0000

    improve: added spacing, added spacing, added separator in index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import { registerRootComponent } from 'expo';
 
+
 import App from './App';
 
 
 // tweaked: util
+
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+
 registerRootComponent(App);
