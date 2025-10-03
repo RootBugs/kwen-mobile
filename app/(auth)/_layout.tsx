25f9803f9c0f285b4c6ca55e7f012342a12206e3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-03 15:11:50 +0000

    improve: added separator, annotated TODO, added separator in _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,12 @@
 import { Stack } from 'expo-router';
 
-
 export default function AuthLayout() {
+
   return (
     <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>
-      <Stack.Screen name="login" />
-
-      <Stack.Screen name="register" />  // review: refactor
+      <Stack.Screen name="login" />  // TODO: validation
+      <Stack.Screen name="register" />
     </Stack>
+
   );
 }
