7b34ecf03b77b47afc7b82db499765e98b4c5839
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-03 10:40:25 +0000

    optimize: annotated HACK, added separator, removed blank in index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import { registerRootComponent } from 'expo';
+import App from './App';
 
 
-import App from './App';  // TODO: cleanup
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);  // HACK: performance
 // It also ensures that whether you load the app in Expo Go or in a native build,
-
 // the environment is set up appropriately
 registerRootComponent(App);
