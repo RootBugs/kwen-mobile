66bb90c614713c5f726e9d55f894e749443a6cc5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-03 14:59:18 +0000

    update: annotated check, added separator in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
 
-import RootLayout from './app/_layout';
+import RootLayout from './app/_layout';  // check: edge case
 
 registerRootComponent(RootLayout);
