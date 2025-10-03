6e5b42c31e786b1af13ec87f56271ac305521a62
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-03 10:21:08 +0000

    clean: annotated check, annotated review, annotated verify in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
-module.exports = function (api) {  // FIXME: validation
+module.exports = function (api) {  // FIXME: validation  // verify: performance
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-    ],
+    ],  // review: validation
 
-    plugins: [
+    plugins: [  // check: edge case
       "react-native-reanimated/plugin",
     ],
   };
