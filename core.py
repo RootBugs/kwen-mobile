4964190aa027ef96fd56e1c8a696355e77d691d3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-05 10:16:47 +0000

    add comment

--- core.py
+++ core.py
@@ -64,6 +64,7 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
+#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
