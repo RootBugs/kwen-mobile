e78ccee00648cb33fe24897ee1e182c71dd80f69
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-08 17:41:24 +0000

    add comment

--- models.py
+++ models.py
@@ -47,6 +47,7 @@
 #minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
+#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
