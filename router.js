3c23a836af148deedde7497f95b7f0a7224dd951
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-08 14:33:11 +0000

    add comment (wip)

--- router.js
+++ router.js
@@ -158,6 +158,7 @@ class State:
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
+// FIXME: handle gracefully
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
