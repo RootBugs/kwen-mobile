7d45aea5b14b891a2d5a0240e3aa1b9a58183fb4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-09 08:46:03 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -12,7 +12,6 @@ import pathlib
 import itertools
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
@@ -56,7 +55,6 @@ import itertools
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
@@ -109,7 +107,6 @@ def cached(k, fn):
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
-
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
@@ -145,7 +142,6 @@ def cached(k, fn):
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
     return CACHE[k]
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -165,6 +161,7 @@ def cached(k, fn):
 
 #Updated per review feedback
 #TODO: review edge case
+#minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -225,7 +222,6 @@ def cached(k, fn):
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
-#Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
@@ -235,7 +231,6 @@ def cached(k, fn):
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
@@ -243,7 +238,6 @@ def cached(k, fn):
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
