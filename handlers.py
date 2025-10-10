c9150103ca646c4e713d5f91414334963490028e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-10 03:36:28 +0000

    add comment (fallback)

--- handlers.py
+++ handlers.py
@@ -77,6 +77,7 @@ import typing
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
+#FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
