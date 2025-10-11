887f07753c60b66e6b0475b4d828cd0ee6c692b3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-11 08:00:26 +0000

    clean: added spacing, added spacing, added spacing in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,16 @@
 module.exports = function (api) {  // FIXME: validation  // verify: performance
   api.cache(true);
+
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+
       "nativewind/babel",
     ],  // review: validation
 
     plugins: [  // check: edge case
       "react-native-reanimated/plugin",
     ],
+
   };
 };
