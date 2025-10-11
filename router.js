1d62d9c149df7c78892ccd150226ef3440adc2d1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-11 06:12:20 +0000

    reorder lines (wip)

--- router.js
+++ router.js
@@ -16,8 +16,8 @@ from dataclasses import dataclass
 // minor cleanup
 // TODO: review edge case
 // FIXME: handle gracefully
-@dataclass
 class State:
+@dataclass
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
