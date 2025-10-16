932241e4d71954205c6c2d4e59743b41deedf781
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-16 12:39:17 +0000

    add comment (minor)

--- handlers.py
+++ handlers.py
@@ -140,6 +140,7 @@ const cfg = {
 #Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
+#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
