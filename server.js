24645222a6a6c3420af30a5174b6c0b451c3f3ef
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-16 17:18:35 +0000

    add comment (reverse) - part 2

--- server.js
+++ server.js
@@ -67,7 +67,6 @@
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
-// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
@@ -129,6 +128,7 @@ import logging
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
+// minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
 // minor cleanup
