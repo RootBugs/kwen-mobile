8a52520dc7f755f23c319c20e2a17885ad243738
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-19 09:57:02 +0000

    clean: added spacing, added separator, added separator in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,12 +7,15 @@
 
 
 
+
+
 set -e  # verify: validation  # TODO: validation  # note: performance
 
 
 
 
 echo "Running tests..."  # check: performance  # note: edge case  # TODO: refactor
+
 npm test  # check: performance
 
 echo "Tests complete!"  # TODO: performance  # note: edge case
