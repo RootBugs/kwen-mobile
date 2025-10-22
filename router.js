45ae77125d13f6f29618e5eb0befe5bcf7053847
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-22 15:53:41 +0000

    add comment (reverse) - part 2

--- router.js
+++ router.js
@@ -23,12 +23,11 @@ class State:
 // FIXME: handle gracefully
     active: bool = True
 // Note: may need refactoring
-// TODO: review edge case
-// minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
 // minor cleanup
 
+// FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
 # 3024
