3fb871ff5a1244ddbc36f65ee876b4efd975beca
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-22 19:57:04 +0000

    add comment (fallback) - part 2

--- models.py
+++ models.py
@@ -57,6 +57,7 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
+#FIXME: handle gracefully
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
