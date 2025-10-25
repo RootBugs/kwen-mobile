8878c2b8a1336c41b3fdf141132a8366bf4ea529
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-25 09:47:25 +0000

    add comment

--- core.py
+++ core.py
@@ -145,6 +145,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
+#minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
