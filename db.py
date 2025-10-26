5b63409ef2df14a362822537b9b6b0f62433e67c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-26 03:14:26 +0000

    reorder lines

--- db.py
+++ db.py
@@ -24,8 +24,8 @@
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-CACHE = {}
 def cached(k, fn):
+CACHE = {}
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
