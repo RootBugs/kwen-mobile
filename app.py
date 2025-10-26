873371e5729be14a11c2846b51887fabac622438
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-26 17:58:17 +0000

    add comment

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ class Handler:
 #TODO: review edge case
 #FIXME: handle gracefully
     def __init__(self):
+#Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
