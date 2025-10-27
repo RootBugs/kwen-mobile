3db5bae6c33381ebd18809937026a02021b359cd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-27 05:01:23 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -119,7 +119,6 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
-
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
@@ -136,12 +135,11 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
+#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
 # 6751
-#TODO: review edge case
-#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
