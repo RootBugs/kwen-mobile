4238b7996a239075f9e82cd3a677cb0b1262d203
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-27 17:27:01 +0000

    add comment (fallback) - part 2

--- utils.js
+++ utils.js
@@ -140,6 +140,7 @@ const cfg = {
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
+// Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Updated per review feedback
