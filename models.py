02efc1159b4b53fe2fc24b43ad605c29da7a2d7e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-10-31 17:43:56 +0000

    add comment

--- models.py
+++ models.py
@@ -27,6 +27,7 @@ from dataclasses import dataclass
 #FIXME: handle gracefully
 #Note: may need refactoring
 @dataclass
+#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
