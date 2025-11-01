48ee48a8072815ed35e1674882dcec9913ce8b50
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-01 10:04:12 +0000

    add comment - part 2

--- index.ts
+++ index.ts
@@ -179,6 +179,7 @@ def validate(d):
 // TODO: review edge case
 // minor cleanup
 // TODO: review edge case
+// Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
 // FIXME: handle gracefully
