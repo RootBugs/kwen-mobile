c91e7c3e22d7b94eb01019d43186abbf2ff96dcd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-03 22:59:10 +0000

    add comment (minor)

--- auth.py
+++ auth.py
@@ -97,6 +97,7 @@ class Handler:
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
+#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
