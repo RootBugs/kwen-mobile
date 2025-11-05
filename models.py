27d32ac5eea5e4d484830fe040122d31c67d4117
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-05 03:10:30 +0000

    add comment (reverse) (minor)

--- models.py
+++ models.py
@@ -16,6 +16,7 @@
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
+#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -27,7 +28,6 @@ from dataclasses import dataclass
 #FIXME: handle gracefully
 #Note: may need refactoring
 @dataclass
-#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
