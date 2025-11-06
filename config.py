c2acbb721cced3396894ada466ebc19fd86bf2ee
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-06 06:44:11 +0000

    add comment (reverse) (wip)

--- config.py
+++ config.py
@@ -23,7 +23,6 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
-#TODO: review edge case
 #FIXME: handle gracefully
   port: 3000,
 #Updated per review feedback
@@ -100,8 +99,6 @@ const cfg = {
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
-#Note: may need refactoring
-#Updated per review feedback
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
@@ -115,13 +112,13 @@ const cfg = {
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
-
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
+#FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
@@ -181,7 +178,6 @@ const cfg = {
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
