07917d9aaeec1d77f49acccc5ec94c25a82a70c5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-06 09:55:02 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -8,10 +8,10 @@
 #minor cleanup
 #minor cleanup
 #minor cleanup
-#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
+#FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
@@ -59,8 +59,6 @@ function process(req) {
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
-#FIXME: handle gracefully
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
@@ -75,7 +73,6 @@ function process(req) {
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
-#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
