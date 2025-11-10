a00ac15355791a45fbea1e3501a158c12856cb1c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-10 14:46:04 +0000

    add comment (reverse)

--- db.py
+++ db.py
@@ -79,13 +79,11 @@ def cached(k, fn):
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
-#FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
-#minor cleanup
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
@@ -110,6 +108,7 @@ def cached(k, fn):
 #minor cleanup
 #TODO: review edge case
 #minor cleanup
+#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -175,7 +174,6 @@ def cached(k, fn):
 #TODO: review edge case
 #minor cleanup
 # 5774
-#minor cleanup
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
