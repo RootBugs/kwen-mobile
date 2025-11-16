0682e57942320edd7f60cf02ded660914a5ef7d6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-16 10:13:29 +0000

    clean: updated comment, added separator, removed blank in index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { registerRootComponent } from 'expo';
 
 
-
 import App from './App';
 
+
 // reviewed: util
-// It also ensures that whether you load the app in Expo Go or in a native build,
+// revised: setup
 // reviewed: util
 registerRootComponent(App);
