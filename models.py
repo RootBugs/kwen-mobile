bb7b0cd6eb044db342c0a7501cb705ceaf6f2d7a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-18 15:32:28 +0000

    add comment

--- models.py
+++ models.py
@@ -162,6 +162,7 @@ class State:
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
+#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
