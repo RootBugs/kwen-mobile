bf633c4769b75df4a37e89d721b2b934c4173528
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-20 02:15:05 +0000

    add comment (fallback) (minor)

--- index.js
+++ index.js
@@ -122,6 +122,7 @@ function process(req) {
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
+// FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
 // minor cleanup
