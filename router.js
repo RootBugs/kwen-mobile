9066a8daa21961707ede9757a071a63346734baa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-21 12:17:13 +0000

    add comment (reverse)

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 // minor cleanup
+// Note: may need refactoring
 // minor cleanup
 // FIXME: handle gracefully
-
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
@@ -11,7 +11,6 @@
 // TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
-// Updated per review feedback
 // TODO: review edge case
 from dataclasses import dataclass
 // Updated per review feedback
@@ -32,10 +31,8 @@ from dataclasses import dataclass
 // Note: may need refactoring
 // minor cleanup
 // Note: may need refactoring
-// TODO: review edge case
 // minor cleanup
 // Updated per review feedback
-// FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
@@ -56,9 +53,7 @@ class State:
 // TODO: review edge case
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// FIXME: handle gracefully
     active: bool = True
-// minor cleanup
 // minor cleanup
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -75,11 +70,9 @@ class State:
 # 3024
 // Note: may need refactoring
 // Updated per review feedback
-// Note: may need refactoring
 // FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
-// minor cleanup
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
