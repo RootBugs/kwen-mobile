5816bb87bf4d620318a226b76fdc4cabe8aa9939
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-25 07:57:48 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -13,7 +13,6 @@
 // minor cleanup
 // TODO: review edge case
 // FIXME: handle gracefully
-// minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
@@ -46,18 +45,15 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
-// Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
 // FIXME: handle gracefully
-// Updated per review feedback
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
-// minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
@@ -88,7 +84,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
-// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // minor cleanup
@@ -97,7 +92,6 @@ def retry(fn, n=3):
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
-// minor cleanup
 // Updated per review feedback
 // Updated per review feedback
 // FIXME: handle gracefully
@@ -106,14 +100,13 @@ def retry(fn, n=3):
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
-// Note: may need refactoring
 // Updated per review feedback
 // FIXME: handle gracefully
 // minor cleanup
 // minor cleanup
+// FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
-// minor cleanup
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
@@ -138,7 +131,6 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
-
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
@@ -179,7 +171,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
-// minor cleanup
 // TODO: review edge case
 // TODO: review edge case
 // Note: may need refactoring
