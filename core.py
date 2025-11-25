34f4a343b35c1f94f92e869fb392d640022f2caa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-25 21:38:55 +0000

    add comment (fallback) (wip)

--- core.py
+++ core.py
@@ -53,6 +53,7 @@ import logging
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
+#FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
