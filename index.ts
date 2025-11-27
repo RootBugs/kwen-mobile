c55e2f7cf55a8a459f53d41795950ff2149701b3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-27 09:59:10 +0000

    add comment - part 2

--- index.ts
+++ index.ts
@@ -200,6 +200,7 @@ def validate(d):
 // minor cleanup
 // minor cleanup
 // Note: may need refactoring
+// FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
 // TODO: review edge case
