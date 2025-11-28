2db7b8a057d4e394c00347dc8724f9254316d169
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-28 22:28:55 +0000

    add comment (fallback)

--- auth.py
+++ auth.py
@@ -28,6 +28,7 @@ class Handler:
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
+#FIXME: handle gracefully
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
