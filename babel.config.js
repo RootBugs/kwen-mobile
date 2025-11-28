8683750c3479351d2ab71ddb7c9e9755d159993c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-11-28 17:18:09 +0000

    refactor: annotated FIXME, added spacing, added spacing in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
-
-
   return {
+
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-
     ],
-
     plugins: [
+
       "react-native-reanimated/plugin",
     ],
-
-
   };
-};
+};  // FIXME: edge case
