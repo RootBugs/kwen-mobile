375a26a5fbe60a7b149849b9337d7dde50c1aefa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-01 16:05:00 +0000

    add comment (fallback) - part 2

--- models.py
+++ models.py
@@ -114,6 +114,7 @@ class State:
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
+#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
