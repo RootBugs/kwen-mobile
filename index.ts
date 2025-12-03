35093f5a6fa3a150414ab2b618a54e6ed1272c3e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-03 10:30:42 +0000

    add comment (fallback) (minor)

--- index.ts
+++ index.ts
@@ -15,6 +15,7 @@ def validate(d):
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
+// Note: may need refactoring
 // FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
