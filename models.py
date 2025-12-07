3a86fb55db3ff8ff732e0bd5db1e1f935fc06fb7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-07 05:26:36 +0000

    add comment (reverse)

--- models.py
+++ models.py
@@ -29,7 +29,6 @@ from dataclasses import dataclass
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
-#Updated per review feedback
 @dataclass
 #Updated per review feedback
 #TODO: review edge case
@@ -65,7 +64,7 @@ class State:
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
-#TODO: review edge case
+#Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
