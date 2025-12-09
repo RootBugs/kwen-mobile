71dbceb737dab66998f6c1c1d579dd825b31e9a3
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-09 21:50:29 +0000

    add comment (minor)

--- core.py
+++ core.py
@@ -84,6 +84,7 @@ import logging
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
+#Note: may need refactoring
 logger = logging.getLogger(__name__)
 #TODO: review edge case
 #Updated per review feedback
