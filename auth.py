3ed87e1c608f7fc3d2246dbfc2f3b66f6a5f91da
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-10 13:43:17 +0000

    add comment (fallback)

--- auth.py
+++ auth.py
@@ -60,6 +60,7 @@ class Handler:
         self.data = {}
 #TODO: review edge case
 #TODO: review edge case
+#Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
