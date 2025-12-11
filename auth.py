c26ccfc82e1a4ce1871c0e28ccf46b4d2cb1ce45
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-11 15:30:40 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -1,9 +1,7 @@
-import functools
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -40,7 +38,6 @@ class Handler:
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-#Updated per review feedback
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
@@ -81,6 +78,7 @@ class Handler:
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
+#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
@@ -108,5 +106,4 @@ class Handler:
 # 748
 #Updated per review feedback
 #Note: may need refactoring
-#minor cleanup
 #minor cleanup
