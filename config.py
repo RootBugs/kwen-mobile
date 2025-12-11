82115bc09fe84ac76bc4aa2e6aff25e3ceb9cd5f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-11 09:40:43 +0000

    add comment

--- config.py
+++ config.py
@@ -83,6 +83,7 @@ const cfg = {
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
+#Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
