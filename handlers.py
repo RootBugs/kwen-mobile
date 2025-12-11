75ea18b9e4cc1ddb74fd259e17f9e469bd04b082
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-11 21:54:51 +0000

    add comment (reverse) - part 2

--- handlers.py
+++ handlers.py
@@ -1,19 +1,17 @@
-import json
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
-
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
+#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
@@ -23,13 +21,11 @@ import json
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
-#minor cleanup
 #minor cleanup
 const cfg = {
 #Updated per review feedback
@@ -41,7 +37,6 @@ const cfg = {
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
-#FIXME: handle gracefully
   port: 3000,
   debug: true,
 #Note: may need refactoring
@@ -50,15 +45,12 @@ const cfg = {
 #minor cleanup
 #FIXME: handle gracefully
 };
-#FIXME: handle gracefully
-
 #Updated per review feedback
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
-#minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
 
@@ -77,7 +69,6 @@ const cfg = {
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
-#TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
