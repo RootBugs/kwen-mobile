7ab26d31518362585e7b630b9250987a8b1133fb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-12 11:43:48 +0000

    add comment

--- auth.py
+++ auth.py
@@ -108,6 +108,7 @@ class Handler:
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
+#Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
