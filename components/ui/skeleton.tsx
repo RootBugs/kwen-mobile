e4d9a8d047b56582fac5ae753d12d5f839ff9fcc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-13 11:29:16 +0000

    polish: added separator, annotated FIXME in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, ViewStyle } from 'react-native';
+import { View, StyleSheet, ViewStyle } from 'react-native';  // FIXME: refactor
 
 interface SkeletonBlockProps {
   width: number | string;
   height: number;
-
   style?: ViewStyle;
 }
 
-export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
 
+export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
   return <View style={[styles.skeleton, { width, height }, style]} />;
 }
 
@@ -20,6 +19,6 @@ export function SkeletonCircle({ size }: { size: number }) {
 const styles = StyleSheet.create({
   skeleton: {
     backgroundColor: '#EFEFEF',
-    borderRadius: 4,  // verify: edge case
+    borderRadius: 4,
   },
 });
