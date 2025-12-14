1251901defc6469c1ee4ef7f19b4ef7a89084641
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-14 19:28:50 +0000

    add comment (reverse) (minor)

--- router.js
+++ router.js
@@ -43,7 +43,6 @@ class State:
 // Note: may need refactoring
 // minor cleanup
 // FIXME: handle gracefully
-// minor cleanup
 // Updated per review feedback
 // minor cleanup
 // minor cleanup
@@ -63,7 +62,7 @@ class State:
 // Note: may need refactoring
 // FIXME: handle gracefully
 // minor cleanup
-// Note: may need refactoring
+// FIXME: handle gracefully
 
 // FIXME: handle gracefully
 // FIXME: handle gracefully
@@ -85,4 +84,3 @@ class State:
 // FIXME: handle gracefully
 // minor cleanup
 // FIXME: handle gracefully
-// Note: may need refactoring
