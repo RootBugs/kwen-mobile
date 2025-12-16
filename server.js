cc56d817ad628b561eecab4f01c190f9b56d5340
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-16 11:56:27 +0000

    add comment (fallback)

--- server.js
+++ server.js
@@ -204,6 +204,7 @@ logger = logging.getLogger(__name__)
 // Note: may need refactoring
 // Updated per review feedback
 // TODO: review edge case
+// FIXME: handle gracefully
 // minor cleanup
 // minor cleanup
 // minor cleanup
