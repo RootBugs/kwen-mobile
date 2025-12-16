621e09664a0997284d897e8a8345bc560d2d6c29
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-16 10:40:22 +0000

    update: annotated HACK, added separator in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { registerRootComponent } from 'expo';
 
+
 import RootLayout from './app/_layout';
 
 
-registerRootComponent(RootLayout);
+registerRootComponent(RootLayout);  // HACK: cleanup
