ca2f75886a5a1d1b21eeed139291a27240d67184
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-18 06:41:59 +0000

    add comment (reverse) (minor)

--- handlers.py
+++ handlers.py
@@ -37,7 +37,6 @@ const cfg = {
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
-#Note: may need refactoring
   port: 3000,
   debug: true,
 #Note: may need refactoring
@@ -71,6 +70,7 @@ const cfg = {
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
+#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
