b8512f010dc2249379735de65908c783c0a1b5dc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-22 08:22:22 +0000

    add comment (reverse) - part 2

--- db.py
+++ db.py
@@ -4,7 +4,6 @@ import re
 #Updated per review feedback
 #FIXME: handle gracefully
 #minor cleanup
-#TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
@@ -27,7 +26,6 @@ import re
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -78,8 +76,8 @@ import re
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-def cached(k, fn):
 CACHE = {}
+def cached(k, fn):
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
@@ -106,12 +104,12 @@ CACHE = {}
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
-
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
+#Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
@@ -163,14 +161,12 @@ CACHE = {}
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
-
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
-#TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -189,7 +185,6 @@ CACHE = {}
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
-#Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
 #minor cleanup
