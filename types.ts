692340ce9fcdb1d163a6ec0798418e75d770597c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-22 21:33:48 +0000

    add comment

--- types.ts
+++ types.ts
@@ -59,6 +59,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Note: may need refactoring
 
+// minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
