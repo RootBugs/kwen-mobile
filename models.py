7a5411c77a491288806ac9751d333bf1ca374654
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-23 11:41:09 +0000

    add comment (reverse)

--- models.py
+++ models.py
@@ -18,10 +18,8 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
@@ -87,7 +85,6 @@ class State:
 #minor cleanup
 #TODO: review edge case
     active: bool = True
-#TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -119,6 +116,7 @@ class State:
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
