ce08cff0f3bbabd7ae9f71ae7d236c1f54187a99
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-23 18:05:57 +0000

    add comment (fallback)

--- types.ts
+++ types.ts
@@ -44,6 +44,7 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // TODO: review edge case
 // FIXME: handle gracefully
+// minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
 // Note: may need refactoring
