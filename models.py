188ee155901d4d3382746e2ca2750b94cde75832
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-23 11:28:26 +0000

    add comment - part 2

--- models.py
+++ models.py
@@ -18,6 +18,7 @@
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
