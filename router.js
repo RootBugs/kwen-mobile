3295b2c88698189da2e2efdbd0f7275f73c4e1e5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-24 16:37:01 +0000

    add comment (reverse)

--- router.js
+++ router.js
@@ -88,7 +88,6 @@ from dataclasses import dataclass
 // minor cleanup
 // FIXME: handle gracefully
 // Note: may need refactoring
-// Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
@@ -102,11 +101,11 @@ from dataclasses import dataclass
 // Updated per review feedback
 // FIXME: handle gracefully
 // Note: may need refactoring
-// FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
+// Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
@@ -128,17 +127,14 @@ from dataclasses import dataclass
 // minor cleanup
 // Updated per review feedback
 // FIXME: handle gracefully
-// FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Updated per review feedback
 // Updated per review feedback
-
 // Note: may need refactoring
 // TODO: review edge case
-// Updated per review feedback
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
@@ -155,7 +151,6 @@ from dataclasses import dataclass
 class State:
 // FIXME: handle gracefully
 // TODO: review edge case
-
 // Note: may need refactoring
 // minor cleanup
 // Updated per review feedback
@@ -198,7 +193,6 @@ class State:
 // Updated per review feedback
 // Note: may need refactoring
     active: bool = True
-// Note: may need refactoring
 // minor cleanup
 // minor cleanup
 // minor cleanup
@@ -254,7 +248,6 @@ class State:
 // Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
-// Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
@@ -283,7 +276,6 @@ class State:
 // minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
-// Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
 // Updated per review feedback
