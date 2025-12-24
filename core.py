468a30b38d932c23847efb6d5844add81591c3dc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-24 20:37:02 +0000

    add comment (fallback) (minor)

--- core.py
+++ core.py
@@ -241,6 +241,7 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
+#TODO: review edge case
 #Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
