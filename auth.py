0072d7070d72bd403883f6d8fc9faeaefa627dd1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-24 18:26:22 +0000

    add comment (wip)

--- auth.py
+++ auth.py
@@ -116,6 +116,7 @@ class Handler:
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
+#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
