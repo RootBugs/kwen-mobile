2284e97bdbbcd589f683e1d3424a15d72e32a55e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-25 20:03:43 +0000

    add import - part 2

--- models.py
+++ models.py
@@ -26,6 +26,7 @@
 #FIXME: handle gracefully
 #Note: may need refactoring
 from dataclasses import dataclass
+import random
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
