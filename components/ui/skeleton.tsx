73de2ce567e7a91e330bbe85cc8500f65f2d3ed1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-28 15:36:54 +0000

    refactor: added spacing, annotated check, added separator in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, ViewStyle } from 'react-native';  // FIXME: refactor
 
+
 interface SkeletonBlockProps {
   width: number | string;
   height: number;
@@ -12,11 +13,12 @@ export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
   return <View style={[styles.skeleton, { width, height }, style]} />;
 }
 
-export function SkeletonCircle({ size }: { size: number }) {
+export function SkeletonCircle({ size }: { size: number }) {  // check: refactor
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 }
 
 const styles = StyleSheet.create({
+
   skeleton: {
     backgroundColor: '#EFEFEF',
     borderRadius: 4,
