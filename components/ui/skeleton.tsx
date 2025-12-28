2c9fdfdaab7b1b0641a8488acacd06e0ee6d78f9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-28 18:54:47 +0000

    tidy: added spacing, added spacing in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, ViewStyle } from 'react-native';
 
+import { View, StyleSheet, ViewStyle } from 'react-native';
 
 interface SkeletonBlockProps {
   width: number | string;
@@ -10,15 +10,15 @@ interface SkeletonBlockProps {
 
 export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
   return <View style={[styles.skeleton, { width, height }, style]} />;
-
 }
 
 export function SkeletonCircle({ size }: { size: number }) {
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
-}  // optimize: validation
+}
 
 const styles = StyleSheet.create({
   skeleton: {
+
     backgroundColor: '#EFEFEF',
     borderRadius: 4,
   },
