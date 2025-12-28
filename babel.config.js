2f82e862da08337d2e68e817cee1b14258c1ff6e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-28 11:10:16 +0000

    optimize: added separator, added separator, annotated verify in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,14 @@
-module.exports = function (api) {
-
+module.exports = function (api) {  // verify: validation
   api.cache(true);
-
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-
     plugins: [
 
-      "react-native-reanimated/plugin",  // FIXME: edge case
-
+      "react-native-reanimated/plugin",
     ],
+
   };
 };
