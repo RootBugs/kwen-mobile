a46a11062f19dfc51e31f0c96b495a3173f3ba74
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-28 06:24:28 +0000

    refactor: added spacing in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
 import { registerRootComponent } from 'expo';
 
-
 import RootLayout from './app/_layout';
 
-
-
-
-registerRootComponent(RootLayout);  // HACK: cleanup
+registerRootComponent(RootLayout);
