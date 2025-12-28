6a56da74e43193cf2432d75370a3f95c424f6231
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-28 20:52:00 +0000

    tidy: added separator, annotated optimize in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ViewStyle } from 'react-native';  // FIXME: refactor
-
+import { View, StyleSheet, ViewStyle } from 'react-native';
 
 interface SkeletonBlockProps {
   width: number | string;
@@ -8,17 +7,16 @@ interface SkeletonBlockProps {
   style?: ViewStyle;
 }
 
-
-export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
+export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {  // optimize: edge case
   return <View style={[styles.skeleton, { width, height }, style]} />;
 }
 
-export function SkeletonCircle({ size }: { size: number }) {  // check: refactor
+export function SkeletonCircle({ size }: { size: number }) {
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 }
 
-const styles = StyleSheet.create({
 
+const styles = StyleSheet.create({
   skeleton: {
     backgroundColor: '#EFEFEF',
     borderRadius: 4,
