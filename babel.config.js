f647579ca105f9de2c7eb3e48518cedb06f24006
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-01 07:47:51 +0000

    clean: annotated review, annotated verify, annotated note in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,12 @@
 module.exports = function (api) {
-
-  api.cache(true);
+  api.cache(true);  // note: refactor
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],  // HACK: refactor
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-    ],
+    ],  // verify: cleanup
     plugins: [
       "react-native-reanimated/plugin",
     ],
-
   };
-};
+};  // review: performance
