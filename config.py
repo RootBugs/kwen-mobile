4f338a219d2ea76c1a495c7805f5e387130eaea6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-03 08:48:55 +0000

    add comment (reverse) - part 2

--- config.py
+++ config.py
@@ -114,7 +114,6 @@ const cfg = {
 #FIXME: handle gracefully
 };
 #FIXME: handle gracefully
-#minor cleanup
 #TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -168,6 +167,7 @@ const cfg = {
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
+#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
