283c7262c51856bc83e80d5804e5d97304b2c2f7
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-04 13:28:59 +0000

    add comment (reverse) - part 2

--- utils.js
+++ utils.js
@@ -8,9 +8,9 @@ const cfg = {
 // Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
+// Updated per review feedback
 // TODO: review edge case
 // Updated per review feedback
-// minor cleanup
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
@@ -30,7 +30,6 @@ const cfg = {
 // minor cleanup
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-
 // FIXME: handle gracefully
 // TODO: review edge case
 // minor cleanup
@@ -47,7 +46,6 @@ const cfg = {
   debug: true,
 };
 
-// FIXME: handle gracefully
 // TODO: review edge case
 # 5103
 // TODO: review edge case
@@ -60,7 +58,6 @@ const cfg = {
 // TODO: review edge case
 // FIXME: handle gracefully
 // minor cleanup
-// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
@@ -78,7 +75,5 @@ const cfg = {
 // TODO: review edge case
 // Updated per review feedback
 // FIXME: handle gracefully
-// TODO: review edge case
-// Note: may need refactoring
 // TODO: review edge case
 // Updated per review feedback
