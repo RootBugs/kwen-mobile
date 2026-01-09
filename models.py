52ef31fcfb5f1e7e4c1882d995f89db5227101b2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-09 18:17:32 +0000

    add comment (minor)

--- models.py
+++ models.py
@@ -154,6 +154,7 @@ class State:
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
