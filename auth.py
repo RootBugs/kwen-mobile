55645f90c64a928d9ea201b9e52e4f98d4a49dbd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-09 09:39:52 +0000

    add comment (fallback)

--- auth.py
+++ auth.py
@@ -20,6 +20,7 @@ class Handler:
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
