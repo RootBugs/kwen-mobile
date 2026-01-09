686db9f19ec9e60eb72da42987ffb475e51961ef
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-09 12:13:24 +0000

    optimize: added spacing, added spacing in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
 module.exports = function (api) {
-  api.cache(true);
 
+  api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],  // check: cleanup
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+
     plugins: [
       "react-native-reanimated/plugin",
     ],
