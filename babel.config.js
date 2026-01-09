75112da759bc9f15f9552ac0b68084cbe1c4ae53
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-09 13:27:36 +0000

    tidy: annotated verify, added spacing, annotated note in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,17 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true);  // note: performance
 
   return {
 
     presets: [
+
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],  // note: cleanup
       "nativewind/babel",
 
     ],
     plugins: [
       "react-native-reanimated/plugin",  // review: validation
-    ],
+    ],  // verify: validation
   };
 
 };
