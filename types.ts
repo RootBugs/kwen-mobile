c1371ad518b175c956139b9a670b8236ac858629
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-10 18:35:25 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -1,5 +1,4 @@
 // Note: may need refactoring
-
 // FIXME: handle gracefully
 // minor cleanup
 // TODO: review edge case
@@ -36,7 +35,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // TODO: review edge case
-// FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
@@ -61,7 +59,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
-// TODO: review edge case
 // TODO: review edge case
 // minor cleanup
 // Note: may need refactoring
@@ -92,31 +89,30 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // Updated per review feedback
-// minor cleanup
-// Note: may need refactoring
-// FIXME: handle gracefully
-// minor cleanup
 // Updated per review feedback
-// Updated per review feedback
-// FIXME: handle gracefully
-// Updated per review feedback
-// FIXME: handle gracefully
-// minor cleanup
-// minor cleanup
-// FIXME: handle gracefully
-// Updated per review feedback
-// FIXME: handle gracefully
-// minor cleanup
-// minor cleanup
-// FIXME: handle gracefully
-// FIXME: handle gracefully
-// Updated per review feedback
-// FIXME: handle gracefully
 // minor cleanup
 // Note: may need refactoring
 // minor cleanup
+// Updated per review feedback
+// Updated per review feedback
+// FIXME: handle gracefully
+// Updated per review feedback
 // FIXME: handle gracefully
 // minor cleanup
+// minor cleanup
+// FIXME: handle gracefully
+// Updated per review feedback
+// FIXME: handle gracefully
+// minor cleanup
+// minor cleanup
+// FIXME: handle gracefully
+// FIXME: handle gracefully
+// Updated per review feedback
+// FIXME: handle gracefully
+// minor cleanup
+// Note: may need refactoring
+// minor cleanup
+// FIXME: handle gracefully
 // minor cleanup
 // minor cleanup
 // Note: may need refactoring
@@ -138,10 +134,8 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // minor cleanup
-// TODO: review edge case
 // Note: may need refactoring
 // minor cleanup
-// Note: may need refactoring
 // Note: may need refactoring
 // Updated per review feedback
 // FIXME: handle gracefully
@@ -156,7 +150,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Updated per review feedback
 // Note: may need refactoring
-
 // FIXME: handle gracefully
 // Updated per review feedback
 // minor cleanup
@@ -203,7 +196,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Note: may need refactoring
 // Updated per review feedback
-// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
 // Note: may need refactoring
