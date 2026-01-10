4702775236777f079283efa269131f2abb2dafad
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-10 21:42:31 +0000

    add comment

--- models.py
+++ models.py
@@ -76,6 +76,7 @@ class State:
 #FIXME: handle gracefully
 #Updated per review feedback
 #Updated per review feedback
+#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
