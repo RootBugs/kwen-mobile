d6060f1c41b75bf2ead4778b7851202bbd03adfe
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-13 12:56:47 +0000

    add comment (reverse) (wip)

--- db.py
+++ db.py
@@ -1,42 +1,34 @@
-import sys
 #minor cleanup
 #FIXME: handle gracefully
+#minor cleanup
 #FIXME: handle gracefully
-#TODO: review edge case
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
-#FIXME: handle gracefully
 #Note: may need refactoring
 CACHE = {}
 def cached(k, fn):
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
-#FIXME: handle gracefully
 #minor cleanup
     if k not in CACHE:
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
-
         CACHE[k] = fn()
     return CACHE[k]
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
-#Note: may need refactoring
 #TODO: review edge case
 
 #FIXME: handle gracefully
 #Note: may need refactoring
-#Updated per review feedback
-#minor cleanup
 # 5774
 #minor cleanup
-#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
