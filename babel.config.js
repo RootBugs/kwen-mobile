53b678d184d34269f03c2142ad05458f23f84931
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-14 15:21:58 +0000

    tidy: added separator, annotated HACK, annotated verify in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,14 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true);  // verify: refactor
 
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],  // note: refactor
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],  // note: refactor  // HACK: edge case
       "nativewind/babel",
     ],
     plugins: [
       "react-native-reanimated/plugin",
+
     ],  // note: refactor
   };
 };
