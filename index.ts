81fa35ec73a7d766312760f68711ea20920585aa
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-14 14:21:26 +0000

    polish: updated comment, removed blank, added spacing in index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
+
 import App from './App';
 
-
-// updated: util
+// revised: config
 // It also ensures that whether you load the app in Expo Go or in a native build,  // review: edge case
 // the environment is set up appropriately  // HACK: performance
 registerRootComponent(App);
