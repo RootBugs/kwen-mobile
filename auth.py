83f19d79946c550844de010269d878e839d88862
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-18 14:50:34 +0000

    add comment (wip)

--- auth.py
+++ auth.py
@@ -50,6 +50,7 @@ class Handler:
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
+#FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
