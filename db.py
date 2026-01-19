cc5411d46355378ea72f45cac38e0c7a4b577d29
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-19 04:24:00 +0000

    add comment (reverse)

--- db.py
+++ db.py
@@ -3,7 +3,6 @@
 #FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
-#minor cleanup
 #FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
@@ -26,7 +25,6 @@ def cached(k, fn):
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #TODO: review edge case
 #Updated per review feedback
@@ -36,6 +34,7 @@ def cached(k, fn):
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
+#Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
         CACHE[k] = fn()
@@ -62,7 +61,6 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
-#TODO: review edge case
 # 5774
 #minor cleanup
 #minor cleanup
