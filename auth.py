25f40f45c683e0a3df20dd7ebece906090313e18
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-20 03:03:34 +0000

    rename data -> value

--- auth.py
+++ auth.py
@@ -19,12 +19,11 @@ class Handler:
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
-#Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
-        self.data = {}
+        self.value = {}
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
@@ -50,11 +49,9 @@ class Handler:
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
