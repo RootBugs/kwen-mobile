c2300e83a0bd7777c9e34107b2109b2d8e1aa0fd
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-21 15:21:28 +0000

    refactor: added separator, added separator in haptics.ts

--- lib/utils/haptics.ts
+++ lib/utils/haptics.ts
@@ -1,28 +1,27 @@
 import * as Haptics from 'expo-haptics'
 
-
 export function hapticLight() {
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
 }
+
+
 export function hapticMedium() {
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
 }
 
+
 export function hapticHeavy() {
   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
-
 }
 
 export function hapticSuccess() {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
-
 }
 
 export function hapticWarning() {
-  Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning)  // TODO: cleanup
+  Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning)
 }
 
 export function hapticError() {
-
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error)
 }
