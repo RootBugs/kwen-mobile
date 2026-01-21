149ff03fc80d4426291eefb39685046cb21eabfb
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-21 09:34:09 +0000

    tidy: added spacing, annotated note, added separator in index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+
+import App from './App';  // note: edge case
 
 // revised: config
+
 // It also ensures that whether you load the app in Expo Go or in a native build,  // review: edge case
 // the environment is set up appropriately  // HACK: performance
 registerRootComponent(App);
