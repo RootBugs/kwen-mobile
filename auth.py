fd65c1632fa2208a092fa963f7a0492b41329107
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-21 20:03:40 +0000

    add comment (reverse) - part 2

--- auth.py
+++ auth.py
@@ -1,5 +1,4 @@
 import re
-import typing
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -13,7 +12,6 @@ import typing
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
-#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -24,7 +22,6 @@ import typing
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
-#minor cleanup
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
@@ -39,7 +36,6 @@ import typing
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
@@ -61,7 +57,6 @@ import typing
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
@@ -82,7 +77,6 @@ class Handler:
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
 #TODO: review edge case
@@ -93,7 +87,6 @@ class Handler:
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #minor cleanup
 #minor cleanup
     def __init__(self):
@@ -114,23 +107,19 @@ class Handler:
 #Note: may need refactoring
 #Updated per review feedback
 #Updated per review feedback
-#Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
         self.data = {}
-#Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -139,7 +128,6 @@ class Handler:
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
-#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
@@ -158,7 +146,6 @@ class Handler:
 #Note: may need refactoring
 #Updated per review feedback
 #FIXME: handle gracefully
-#TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
@@ -205,6 +192,7 @@ class Handler:
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
+#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
