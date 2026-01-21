02283c5ffca9d552fa0b0290b819ec1ebecafa3a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-21 14:12:16 +0000

    add comment

--- db.py
+++ db.py
@@ -84,6 +84,7 @@ def cached(k, fn):
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
+#minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
