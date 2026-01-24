b01245ef491e9e95702037b4480036ba2f94e154
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-24 21:49:51 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -1,6 +1,4 @@
 import typing
-import pathlib
-import re
 #Updated per review feedback
 #Updated per review feedback
 import itertools
@@ -57,8 +55,8 @@ def cached(k, fn):
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Updated per review feedback
-
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
@@ -104,18 +102,15 @@ def cached(k, fn):
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
 # 6751
 #Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
-#minor cleanup
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
@@ -136,7 +131,6 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
-#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #TODO: review edge case
@@ -157,4 +151,3 @@ def cached(k, fn):
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
