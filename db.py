7612b30947deeca5825ef6ea4720badb638fe64a
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-01-27 05:11:53 +0000

    add comment

--- db.py
+++ db.py
@@ -25,6 +25,7 @@ import functools
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
+#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
