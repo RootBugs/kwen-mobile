36a4ae20dd7083dcce063880f0b462dc5b04525b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-02 10:16:11 +0000

    add comment (reverse)

--- router.js
+++ router.js
@@ -29,7 +29,6 @@
 // Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
-// TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
 // minor cleanup
@@ -107,9 +106,9 @@ class State:
 // TODO: review edge case
 // Note: may need refactoring
 // Note: may need refactoring
-// Note: may need refactoring
 // minor cleanup
 // minor cleanup
+// FIXME: handle gracefully
 // FIXME: handle gracefully
 // minor cleanup
 // FIXME: handle gracefully
@@ -128,7 +127,6 @@ class State:
 // minor cleanup
 // minor cleanup
 // minor cleanup
-// Note: may need refactoring
 // Note: may need refactoring
 // Note: may need refactoring
 // FIXME: handle gracefully
@@ -171,7 +169,6 @@ class State:
 // Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
-// Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
 // minor cleanup
