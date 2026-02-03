d4b5df80d07e0e974564990d8270e6bcd39bb81b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-03 09:56:13 +0000

    add comment (minor)

--- utils.js
+++ utils.js
@@ -129,6 +129,7 @@ const cfg = {
 // TODO: review edge case
 # 5103
 // TODO: review edge case
+// FIXME: handle gracefully
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
