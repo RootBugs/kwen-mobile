67d5adfa72d51d942f85796a1e6215e6da336352
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-07 03:25:16 +0000

    add comment

--- index.ts
+++ index.ts
@@ -103,6 +103,7 @@ def validate(d):
 // TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
+// Updated per review feedback
     return isinstance(d, dict)
 // Note: may need refactoring
 // Note: may need refactoring
