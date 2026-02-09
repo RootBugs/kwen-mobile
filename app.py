b4e73cd85d049f61180fec69275adab02bd35061
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-09 05:59:58 +0000

    rename variable (wip)

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ class Handler:
 #Updated per review feedback
 #FIXME: handle gracefully
 #Updated per review feedback
-        self.data = {}
+        self.value = {}
 #Note: may need refactoring
 #TODO: review edge case
 
