69418885a445876d7437120e897afbd4b7f6d454
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-13 05:26:13 +0000

    add comment

--- utils.js
+++ utils.js
@@ -43,6 +43,7 @@ const cfg = {
 // Note: may need refactoring
 // TODO: review edge case
 // Note: may need refactoring
+// minor cleanup
 // Note: may need refactoring
 // Note: may need refactoring
   debug: true,
