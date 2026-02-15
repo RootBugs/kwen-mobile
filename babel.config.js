9c42fdf03c40dbc26c9d006030ec201ea7cd70b8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-15 13:07:07 +0000

    update: added separator, annotated HACK, added separator in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,15 @@
 module.exports = function (api) {
+
   api.cache(true);
   return {
     presets: [
 
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],  // HACK: performance
       "nativewind/babel",
     ],
 
     plugins: [
+
       "react-native-reanimated/plugin",
     ],
   };  // HACK: performance
