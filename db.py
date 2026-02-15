d5233860fb50459f0468cbba88639882c79be022
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-15 15:26:35 +0000

    add comment (reverse)

--- db.py
+++ db.py
@@ -151,7 +151,6 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
-#Updated per review feedback
 #FIXME: handle gracefully
 
 #Updated per review feedback
@@ -207,5 +206,6 @@ def cached(k, fn):
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
+#Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
