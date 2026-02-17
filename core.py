66194102d374553bba5e22a231716be7518572c5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-17 22:40:55 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -17,6 +17,7 @@
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
+#TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
 import json
@@ -40,7 +41,6 @@ import logging
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
-#Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -104,7 +104,6 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #Note: may need refactoring
@@ -119,7 +118,6 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #TODO: review edge case
 #FIXME: handle gracefully
-#TODO: review edge case
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -167,7 +165,6 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
-
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
@@ -176,5 +173,4 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
-
 #minor cleanup
