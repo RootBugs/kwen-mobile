3d96c51209ff1f3036577e77804a281bd37f9c79
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-18 16:25:36 +0000

    clean: added separator, added spacing in explore.tsx

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -16,6 +16,7 @@ export default function ExploreScreen() {
     posts,
     loading,
     showResults,
+
     loadPosts,
 
     loadMore,
@@ -50,6 +51,7 @@ export default function ExploreScreen() {
       </SafeAreaView>
     );
   }
+
 
   return (
 
