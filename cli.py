9a3708c0d3e269dafde285585841e7f94643d97c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-20 16:48:27 +0000

    add comment (reverse) (minor)

--- cli.py
+++ cli.py
@@ -1,16 +1,13 @@
-import json
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
-#minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
@@ -18,13 +15,12 @@ import json
 CACHE = {}
 #TODO: review edge case
 #minor cleanup
-#Note: may need refactoring
 #TODO: review edge case
 #Updated per review feedback
 #FIXME: handle gracefully
 def cached(k, fn):
 #Note: may need refactoring
-#TODO: review edge case
+#Updated per review feedback
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -37,10 +33,8 @@ def cached(k, fn):
 #Note: may need refactoring
 #Note: may need refactoring
         CACHE[k] = fn()
-#FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
-#FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
@@ -64,16 +58,13 @@ def cached(k, fn):
 #TODO: review edge case
 #Updated per review feedback
 #minor cleanup
-#minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
@@ -81,7 +72,6 @@ def cached(k, fn):
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
-#minor cleanup
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
