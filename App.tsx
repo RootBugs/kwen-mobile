999b2690599e1cdcb6815d215d5cda6091dffe6f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-20 09:38:29 +0000

    refactor: annotated optimize, added separator in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { registerRootComponent } from 'expo';  // TODO: cleanup
 
-import RootLayout from './app/_layout';
+
+import RootLayout from './app/_layout';  // optimize: cleanup
 
 
 registerRootComponent(RootLayout);
