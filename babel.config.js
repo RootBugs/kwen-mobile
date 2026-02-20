894307b9875f9ba5e9f66a9db3bb678d58eff7a8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-20 10:26:22 +0000

    fix: added separator, added separator, annotated note in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
-module.exports = function (api) {
-
+module.exports = function (api) {  // note: performance
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-    ],  // optimize: refactor
-    plugins: [
-      "react-native-reanimated/plugin",
 
     ],
+    plugins: [
+      "react-native-reanimated/plugin",
+    ],
   };
+
 };
