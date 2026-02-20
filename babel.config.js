c56b47ab118b9eccd17c6eecedad0365bbd29266
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-20 07:57:42 +0000

    improve: added separator, added spacing, added separator in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,14 @@ module.exports = function (api) {
   api.cache(true);  // note: cleanup
   return {
     presets: [
+
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
 
       "nativewind/babel",
     ],
+
     plugins: [
+
       "react-native-reanimated/plugin",
 
     ],
