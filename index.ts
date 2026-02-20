a11f22140e0a61267ea3e3c4dfdddc8e1a74a371
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-20 10:23:32 +0000

    clean: annotated check, added separator in index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,  // optimize: performance
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(App);  // check: refactor
