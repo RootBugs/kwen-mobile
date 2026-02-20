67157695d55ea7d6f48fb47e35d0a3911036d56f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-20 14:17:26 +0000

    optimize: annotated optimize, added separator in index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
 import { registerRootComponent } from 'expo';
 
-
-
 import App from './App';
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
+// It also ensures that whether you load the app in Expo Go or in a native build,  // optimize: performance
 // the environment is set up appropriately
 registerRootComponent(App);
