8e533e0acd71293d2a5c5f62256f4e702b9362d5
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-21 20:06:02 +0000

    add comment (reverse)

--- utils.js
+++ utils.js
@@ -6,15 +6,12 @@ const cfg = {
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
-// TODO: review edge case
 // Note: may need refactoring
 // Updated per review feedback
   port: 3000,
 // minor cleanup
 // minor cleanup
-// Note: may need refactoring
 // FIXME: handle gracefully
-// TODO: review edge case
 // FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
@@ -23,19 +20,17 @@ const cfg = {
 // minor cleanup
 // TODO: review edge case
 // Note: may need refactoring
-// Note: may need refactoring
-};
   debug: true,
+};
 
 // TODO: review edge case
 # 5103
 // TODO: review edge case
-// TODO: review edge case
 // FIXME: handle gracefully
-// Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
+// Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
