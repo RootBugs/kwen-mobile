5b32e06c8353679fb2e4dab88d759b84b30ea50b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-24 09:27:29 +0000

    add comment (reverse) (minor)

--- db.py
+++ db.py
@@ -42,6 +42,7 @@ import functools
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
+#Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -55,7 +56,6 @@ import functools
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
-#minor cleanup
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
@@ -139,7 +139,6 @@ def cached(k, fn):
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
-#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
