13e23cc38701742b4ceea6eb08623118023971c6
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-28 14:59:36 +0000

    add comment (reverse) (minor)

--- server.js
+++ server.js
@@ -49,7 +49,6 @@
 // Note: may need refactoring
 // TODO: review edge case
 // minor cleanup
-// Updated per review feedback
 // FIXME: handle gracefully
 // minor cleanup
 // FIXME: handle gracefully
@@ -110,6 +109,7 @@ logger = logging.getLogger(__name__)
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // Note: may need refactoring
+// Updated per review feedback
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
