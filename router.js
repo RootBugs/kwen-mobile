5ebd9bab628d30d6c009f477b6e51f0942369ff2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-01 15:34:16 +0000

    add comment

--- router.js
+++ router.js
@@ -222,6 +222,7 @@ class State:
 // Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// Updated per review feedback
 // TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
