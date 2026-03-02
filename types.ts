e7621fb1d07cb1fde4c7cba617abb105612d369b
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-02 11:37:32 +0000

    add comment (minor)

--- types.ts
+++ types.ts
@@ -13,6 +13,7 @@ def retry(fn, n=3):
 // minor cleanup
 // Note: may need refactoring
 // TODO: review edge case
+// minor cleanup
 // Updated per review feedback
 // Updated per review feedback
     for i in range(n):
