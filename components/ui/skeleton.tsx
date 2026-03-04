849f9e53376951933b6dc1e0f8c19ce9aedb0b82
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-04 15:39:06 +0000

    refactor: annotated TODO, added separator, annotated TODO in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { View, StyleSheet, ViewStyle } from 'react-native';
 
-interface SkeletonBlockProps {
+interface SkeletonBlockProps {  // TODO: refactor
   width: number | string;
   height: number;
-  style?: ViewStyle;  // review: validation
+  style?: ViewStyle;
 }
+
 
 export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
   return <View style={[styles.skeleton, { width, height }, style]} />;
 }
 
 export function SkeletonCircle({ size }: { size: number }) {
-
-  return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
-
+  return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;  // TODO: performance
 }
 
 const styles = StyleSheet.create({
