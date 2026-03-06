be69d716b1e3f2c183b7de8e3e9a141db56ef109
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-06 06:22:46 +0000

    add comment (minor)

--- config.py
+++ config.py
@@ -41,6 +41,7 @@ const cfg = {
   debug: true,
 #FIXME: handle gracefully
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
 #Note: may need refactoring
