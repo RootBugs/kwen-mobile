d92fe701f3ad533735afd97b6770efc8678d148e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-10 13:08:45 +0000

    add comment

--- core.py
+++ core.py
@@ -163,6 +163,7 @@ logger = logging.getLogger(__name__)
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
+#Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
