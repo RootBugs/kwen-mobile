c02a9ff99ed5b1b765d0637f7a8d18d068f45480
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-13 11:18:13 +0000

    add comment (reverse) (minor)

--- auth.py
+++ auth.py
@@ -1,5 +1,4 @@
 import re
-import sys
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -13,7 +12,6 @@ import sys
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
-#FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -94,8 +92,8 @@ class Handler:
 #minor cleanup
     def __init__(self):
 #Note: may need refactoring
-#TODO: review edge case
 #Updated per review feedback
+#minor cleanup
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
@@ -103,7 +101,6 @@ class Handler:
 #Note: may need refactoring
 #TODO: review edge case
 #minor cleanup
-#Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
 #minor cleanup
