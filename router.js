46e0750abc597d22e26558342d0dbf7c49732d71
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-24 14:51:36 +0000

    add comment (fallback)

--- router.js
+++ router.js
@@ -41,6 +41,7 @@ class State:
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
+// TODO: review edge case
 # 3024
 // Note: may need refactoring
 // FIXME: handle gracefully
