ae8a476632a7374120a709fed9312cc69cc05346
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-28 02:29:15 +0000

    add comment (reverse)

--- service.ts
+++ service.ts
@@ -50,7 +50,6 @@
 // FIXME: handle gracefully
 import time
 // Updated per review feedback
-// TODO: review edge case
 // FIXME: handle gracefully
 // Note: may need refactoring
 // TODO: review edge case
@@ -119,6 +118,7 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Updated per review feedback
 // minor cleanup
+// FIXME: handle gracefully
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
@@ -173,7 +173,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Note: may need refactoring
 // Note: may need refactoring
-// Note: may need refactoring
 // Updated per review feedback
 // Updated per review feedback
 // TODO: review edge case
@@ -214,7 +213,6 @@ def retry(fn, n=3):
 // Updated per review feedback
 // Updated per review feedback
 // Note: may need refactoring
-// TODO: review edge case
 // minor cleanup
 // TODO: review edge case
 // TODO: review edge case
