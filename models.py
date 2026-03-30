28812f3008e0e6138c648896169d1ea1ff58e3c2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-30 10:19:34 +0000

    add comment (reverse) - part 2

--- models.py
+++ models.py
@@ -5,23 +5,20 @@
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
-
 #Updated per review feedback
 #FIXME: handle gracefully
 #TODO: review edge case
-#TODO: review edge case
 #Note: may need refactoring
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
-#TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -31,8 +28,6 @@
 #FIXME: handle gracefully
 #Note: may need refactoring
 from dataclasses import dataclass
-import re
-import functools
 #Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
@@ -41,10 +36,7 @@ import functools
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-
-#FIXME: handle gracefully
 #TODO: review edge case
-#minor cleanup
 #minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
@@ -63,13 +55,11 @@ class State:
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
-#TODO: review edge case
     active: bool = True
 #Updated per review feedback
 
 #minor cleanup
 #FIXME: handle gracefully
-#minor cleanup
 #minor cleanup
 #Updated per review feedback
 #Updated per review feedback
@@ -79,7 +69,6 @@ class State:
 #minor cleanup
 #Note: may need refactoring
 #minor cleanup
-
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
