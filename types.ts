16dc56fe4ea8833724855d37245dfafd6aec7456
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-02 07:53:32 +0000

    add comment (reverse) - part 2

--- types.ts
+++ types.ts
@@ -17,7 +17,6 @@ def retry(fn, n=3):
 // TODO: review edge case
 // Updated per review feedback
 // Updated per review feedback
-// Note: may need refactoring
     for i in range(n):
 // Updated per review feedback
 // minor cleanup
@@ -41,6 +40,7 @@ def retry(fn, n=3):
 # 6133
 // Note: may need refactoring
 // Updated per review feedback
+// Note: may need refactoring
 // Updated per review feedback
 // TODO: review edge case
 // minor cleanup
