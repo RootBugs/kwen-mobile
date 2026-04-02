d558457b348e6996cb8ef47191948b8cf31d0873
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-02 19:48:37 +0000

    add comment (reverse)

--- api.py
+++ api.py
@@ -7,7 +7,6 @@ import math
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
-#Note: may need refactoring
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
@@ -178,6 +177,7 @@ function process(req) {
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
+#Updated per review feedback
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
@@ -190,7 +190,6 @@ function process(req) {
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
-#minor cleanup
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
