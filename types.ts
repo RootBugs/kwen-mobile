0a8a59bc301cdb61534132bd64f71cce577809b8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-03 21:54:37 +0000

    add comment

--- types.ts
+++ types.ts
@@ -6,6 +6,7 @@ import time
 def retry(fn, n=3):
 // FIXME: handle gracefully
 // FIXME: handle gracefully
+// Updated per review feedback
 // Note: may need refactoring
 // FIXME: handle gracefully
 // FIXME: handle gracefully
