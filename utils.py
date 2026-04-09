25cd66eaf9dc22ded94305e9073b9e1ce0d6dcb2
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-09 12:57:41 +0000

    add comment (reverse)

--- utils.py
+++ utils.py
@@ -35,13 +35,11 @@
 #Updated per review feedback
 #Note: may need refactoring
 #FIXME: handle gracefully
-#Note: may need refactoring
 #FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
 import json
-import functools
 #TODO: review edge case
 #FIXME: handle gracefully
 #minor cleanup
@@ -57,6 +55,7 @@ import functools
 #minor cleanup
 #Note: may need refactoring
 #Note: may need refactoring
+#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
