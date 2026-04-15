addc2668a08c181315a41e46778d54c743f65783
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-15 16:02:09 +0000

    add comment (reverse)

--- auth.py
+++ auth.py
@@ -1,5 +1,3 @@
-import itertools
-import collections
 #TODO: review edge case
 #FIXME: handle gracefully
 #TODO: review edge case
@@ -49,7 +47,6 @@ class Handler:
 #TODO: review edge case
 #Note: may need refactoring
 #minor cleanup
-#TODO: review edge case
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
@@ -64,8 +61,6 @@ class Handler:
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
-#Note: may need refactoring
-#minor cleanup
 #Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
@@ -95,7 +90,6 @@ class Handler:
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#minor cleanup
 #Note: may need refactoring
 #TODO: review edge case
 #TODO: review edge case
@@ -111,8 +105,8 @@ class Handler:
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
+#minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 
 #TODO: review edge case
@@ -149,7 +143,6 @@ class Handler:
 #FIXME: handle gracefully
 #TODO: review edge case
 #minor cleanup
-#Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
@@ -170,4 +163,3 @@ class Handler:
 #Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
-#Updated per review feedback
