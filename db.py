e66c4ac245c8b38badcb4733617eeb6f5c41d816
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-15 21:37:27 +0000

    add comment (reverse) (wip)

--- db.py
+++ db.py
@@ -52,6 +52,7 @@ def cached(k, fn):
 #TODO: review edge case
 #minor cleanup
 #minor cleanup
+#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
@@ -60,30 +61,28 @@ def cached(k, fn):
 #Updated per review feedback
 #Updated per review feedback
 #Updated per review feedback
+#Updated per review feedback
+#TODO: review edge case
+#Updated per review feedback
+#TODO: review edge case
+#Updated per review feedback
+#minor cleanup
+#Note: may need refactoring
+#Updated per review feedback
+#Updated per review feedback
+#minor cleanup
+#minor cleanup
+#Note: may need refactoring
+#TODO: review edge case
+#Updated per review feedback
+#Updated per review feedback
+#Note: may need refactoring
+#Updated per review feedback
+#minor cleanup
 #FIXME: handle gracefully
-#Updated per review feedback
-#TODO: review edge case
-#Updated per review feedback
-#TODO: review edge case
-#Updated per review feedback
-#minor cleanup
-#Note: may need refactoring
-#Updated per review feedback
-#Updated per review feedback
-#minor cleanup
-#minor cleanup
-#Note: may need refactoring
-#TODO: review edge case
-#Updated per review feedback
-#Updated per review feedback
-#Note: may need refactoring
-#Updated per review feedback
-#minor cleanup
-#FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
-
 #minor cleanup
 #Updated per review feedback
     if k not in CACHE:
@@ -119,14 +118,11 @@ def cached(k, fn):
 #minor cleanup
 #minor cleanup
 #minor cleanup
-
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #minor cleanup
 #FIXME: handle gracefully
 
-#Updated per review feedback
-#FIXME: handle gracefully
 #Note: may need refactoring
 #minor cleanup
 #TODO: review edge case
@@ -155,7 +151,6 @@ def cached(k, fn):
 # 5774
 #minor cleanup
 #minor cleanup
-#Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
