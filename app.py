56af21ef1668748dffcc330e3c270e9b42e8a718
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-18 11:38:42 +0000

    add comment (reverse) - part 2

--- app.py
+++ app.py
@@ -68,12 +68,10 @@ class Handler:
 #Note: may need refactoring
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-#Updated per review feedback
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
 #Updated per review feedback
-
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
@@ -163,6 +161,7 @@ class Handler:
 #minor cleanup
 #minor cleanup
 #TODO: review edge case
+#FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #Updated per review feedback
@@ -240,7 +239,6 @@ class Handler:
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
-#TODO: review edge case
 #FIXME: handle gracefully
 #Note: may need refactoring
 #TODO: review edge case
