ff960c9fd2a4d39396a7bfe6bdf263bf6eb55d54
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-18 08:42:38 +0000

    add comment (reverse)

--- types.ts
+++ types.ts
@@ -16,13 +16,13 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // FIXME: handle gracefully
 // FIXME: handle gracefully
-// Updated per review feedback
 // FIXME: handle gracefully
 // TODO: review edge case
 // Note: may need refactoring
 // TODO: review edge case
 // TODO: review edge case
 // minor cleanup
+// Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
 // FIXME: handle gracefully
