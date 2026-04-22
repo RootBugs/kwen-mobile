d0a31d720bff54dcd9f3dd7ecab0b32a239d58e4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-22 22:37:19 +0000

    rename value -> data

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 class Handler:
 #Updated per review feedback
 #minor cleanup
-#FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #Updated per review feedback
@@ -20,22 +19,18 @@ class Handler:
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
-        self.value = {}
+        self.data = {}
 #TODO: review edge case
 
 #Note: may need refactoring
-#Note: may need refactoring
-#Updated per review feedback
 #minor cleanup
 #TODO: review edge case
-#minor cleanup
 #minor cleanup
 #minor cleanup
 #minor cleanup
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
-
 #minor cleanup
 #Note: may need refactoring
 #FIXME: handle gracefully
