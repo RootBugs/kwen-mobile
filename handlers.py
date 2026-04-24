b7de094d58fb1fddb5f55945845e015aa5c288f4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-24 05:29:04 +0000

    add comment (reverse)

--- handlers.py
+++ handlers.py
@@ -15,7 +15,6 @@
 #TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
-#Note: may need refactoring
 #TODO: review edge case
 #Note: may need refactoring
 #Note: may need refactoring
@@ -45,6 +44,7 @@ const cfg = {
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
+#Note: may need refactoring
   port: 3000,
   debug: true,
 #Note: may need refactoring
@@ -55,7 +55,6 @@ const cfg = {
 #TODO: review edge case
 };
 #Updated per review feedback
-#TODO: review edge case
 #minor cleanup
 #minor cleanup
 #FIXME: handle gracefully
@@ -72,7 +71,6 @@ const cfg = {
 #FIXME: handle gracefully
 #minor cleanup
 #Note: may need refactoring
-#TODO: review edge case
 #minor cleanup
 #Updated per review feedback
 #FIXME: handle gracefully
