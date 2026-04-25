1ca06d25a6f3ae276ed8c97cdbd1cc924a4a259c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-25 21:11:03 +0000

    optimize: annotated TODO, removed blank, removed blank in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,14 +12,12 @@
 
 
 
-
-
 set -e  # verify: validation  # TODO: validation  # note: performance  # review: edge case
 
 
 
 echo "Running tests..."  # check: performance  # note: edge case  # TODO: refactor  # FIXME: performance
 
-npm test  # check: performance
+npm test  # check: performance  # TODO: refactor
 
 echo "Tests complete!"  # TODO: performance  # note: edge case  # TODO: edge case
