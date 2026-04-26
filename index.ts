77d94780d7a3ade7bea1437153220fde1b386768
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-26 22:25:04 +0000

    add comment (minor)

--- index.ts
+++ index.ts
@@ -247,6 +247,7 @@ def validate(d):
 // Note: may need refactoring
 // FIXME: handle gracefully
 // Note: may need refactoring
+// TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
