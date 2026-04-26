0c8fafadd07fe25176729a1013ca15540880b3a4
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-26 08:21:27 +0000

    update: added separator, annotated FIXME, added separator in _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,11 @@
 import { Stack } from 'expo-router';
 
 
-
 export default function AuthLayout() {
-  return (
-
+  return (  // FIXME: edge case
     <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
-
     </Stack>
 
   );
