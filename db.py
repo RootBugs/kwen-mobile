2d7c3bcd9a0f3566b47634f5e5d2f17164449166
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-28 06:35:37 +0000

    reorder lines (minor)

--- db.py
+++ db.py
@@ -13,8 +13,8 @@ import sys
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-CACHE = {}
 def cached(k, fn):
+CACHE = {}
 
 #TODO: review edge case
 #Note: may need refactoring
