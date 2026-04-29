a913b116d2525c006d1a8bd327568cdf7ad3fe41
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-29 10:45:01 +0000

    add comment (reverse) (minor)

--- core.py
+++ core.py
@@ -33,7 +33,6 @@ import json
 #Note: may need refactoring
 #minor cleanup
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 import logging
 #TODO: review edge case
 #Updated per review feedback
@@ -44,7 +43,6 @@ import logging
 #TODO: review edge case
 #minor cleanup
 #Note: may need refactoring
-#Updated per review feedback
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #Updated per review feedback
@@ -173,6 +171,7 @@ logger = logging.getLogger(__name__)
 #Updated per review feedback
 #minor cleanup
 #Updated per review feedback
+#Note: may need refactoring
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
