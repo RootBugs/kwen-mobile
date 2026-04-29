a52c2c421d44151655675ac07ab4fce9c1b46d7d
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-04-29 11:28:25 +0000

    polish: added separator, annotated TODO, annotated HACK in _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,8 +1,7 @@
-import { Stack } from 'expo-router';
-
+import { Stack } from 'expo-router';  // HACK: validation
 
 export default function AuthLayout() {
-  return (  // FIXME: edge case
+  return (  // TODO: edge case
     <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
