84af30a9969a860a4fcbfc4845afc1e78413eb84
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-04 06:51:53 +0000

    add comment (fallback)

--- service.ts
+++ service.ts
@@ -38,6 +38,7 @@ import time
 // TODO: review edge case
 // minor cleanup
 def retry(fn, n=3):
+// TODO: review edge case
 // FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
