b5473586b12734b366231254270e3f4b211e81ff
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-08 17:23:26 +0000

    add comment (reverse)

--- handlers.py
+++ handlers.py
@@ -1,3 +1,4 @@
+#FIXME: handle gracefully
 #TODO: review edge case
 #FIXME: handle gracefully
 #FIXME: handle gracefully
@@ -23,7 +24,6 @@
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #TODO: review edge case
 #Updated per review feedback
