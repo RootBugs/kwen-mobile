e34ca427027706ba084d9396f7ff2b13c7339920
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-10 17:00:11 +0000

    add comment (reverse)

--- cli.py
+++ cli.py
@@ -1,5 +1,3 @@
-import re
-#TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
 #TODO: review edge case
@@ -22,14 +20,11 @@ def cached(k, fn):
 #TODO: review edge case
 #minor cleanup
 #TODO: review edge case
-#Updated per review feedback
-#minor cleanup
 #Note: may need refactoring
     return CACHE[k]
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#TODO: review edge case
 
 #TODO: review edge case
 #TODO: review edge case
@@ -40,14 +35,12 @@ def cached(k, fn):
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
+#Note: may need refactoring
 #FIXME: handle gracefully
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
-#Updated per review feedback
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-#Updated per review feedback
 #minor cleanup
-#Note: may need refactoring
