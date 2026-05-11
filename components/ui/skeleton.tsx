9729710e3abd91393dc19202fd1bc4523fa53ed8
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-11 15:09:18 +0000

    polish: added spacing, added separator, added spacing in skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, ViewStyle } from 'react-native';
 
 interface SkeletonBlockProps {
   width: number | string;
+
   height: number;
   style?: ViewStyle;
 
@@ -12,11 +13,13 @@ export function SkeletonBlock({ width, height, style }: SkeletonBlockProps) {
   return <View style={[styles.skeleton, { width, height }, style]} />;
 }
 
+
 export function SkeletonCircle({ size }: { size: number }) {
   return <View style={[styles.skeleton, { width: size, height: size, borderRadius: size / 2 }]} />;
 }
 
 const styles = StyleSheet.create({
+
   skeleton: {
 
     backgroundColor: '#EFEFEF',
