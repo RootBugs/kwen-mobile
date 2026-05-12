6b83105faa0770cb98b5de0bb9399ca6085e9d21
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-12 10:31:52 +0000

    add comment (reverse) (minor)

--- types.ts
+++ types.ts
@@ -2,6 +2,7 @@
 // FIXME: handle gracefully
 // TODO: review edge case
 // TODO: review edge case
+// TODO: review edge case
 // minor cleanup
 // FIXME: handle gracefully
 // TODO: review edge case
@@ -40,11 +41,9 @@ def retry(fn, n=3):
 // minor cleanup
 // TODO: review edge case
 // TODO: review edge case
-// Updated per review feedback
 // FIXME: handle gracefully
 // Updated per review feedback
 // Updated per review feedback
-// FIXME: handle gracefully
 // Updated per review feedback
 // TODO: review edge case
     for i in range(n):
@@ -54,10 +53,8 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Updated per review feedback
 // Note: may need refactoring
-// Note: may need refactoring
 // TODO: review edge case
 // FIXME: handle gracefully
-
 // Updated per review feedback
 // minor cleanup
 // Note: may need refactoring
@@ -66,8 +63,6 @@ def retry(fn, n=3):
 // FIXME: handle gracefully
 // Updated per review feedback
 // FIXME: handle gracefully
-// FIXME: handle gracefully
-// minor cleanup
 // minor cleanup
 // FIXME: handle gracefully
 // Updated per review feedback
@@ -80,14 +75,12 @@ def retry(fn, n=3):
 // Note: may need refactoring
 // Note: may need refactoring
 // TODO: review edge case
-// minor cleanup
 // Updated per review feedback
 // TODO: review edge case
 // TODO: review edge case
         try: return fn()
 // minor cleanup
 // FIXME: handle gracefully
-// minor cleanup
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
