da52d7bdbdcf9f79898f944c53a928b8f45287cf
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-27 13:49:51 +0000

    tidy: added spacing, added separator, added separator in _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,13 +2,16 @@ import { Stack } from 'expo-router';  // HACK: validation
 
 
 
+
 export default function AuthLayout() {
   return (  // TODO: edge case
+
 
 
     <Stack screenOptions={{ headerShown: false, animation: 'slide_from_right' }}>
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
+
     </Stack>
   );
 }
