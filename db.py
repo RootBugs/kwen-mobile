0765025cc08c928cfe5937701e141c08089610f0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-05 22:29:22 +0000

    add comment

--- db.py
+++ db.py
@@ -23,6 +23,7 @@ CACHE = {}
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
+#Note: may need refactoring
 #FIXME: handle gracefully
         CACHE[k] = fn()
     return CACHE[k]
