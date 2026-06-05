97fe71c97629e13d202e1b624438d7a208e3006f
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-05 11:03:10 +0000

    add comment (fallback) (minor)

--- app.py
+++ app.py
@@ -152,6 +152,7 @@ class Handler:
 #minor cleanup
 #TODO: review edge case
 #Note: may need refactoring
+#FIXME: handle gracefully
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
