94c5ed30864aacc3d136de7eb8d768c1eddd8af9
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-07 03:43:09 +0000

    add comment (reverse) (wip)

--- db.py
+++ db.py
@@ -7,12 +7,12 @@
 #Updated per review feedback
 #TODO: review edge case
 #Note: may need refactoring
-def cached(k, fn):
+#Note: may need refactoring
 CACHE = {}
+def cached(k, fn):
 #TODO: review edge case
 #TODO: review edge case
 #minor cleanup
-#Updated per review feedback
 #TODO: review edge case
 #Updated per review feedback
 #Updated per review feedback
@@ -23,7 +23,6 @@ CACHE = {}
 #FIXME: handle gracefully
 #Updated per review feedback
 #minor cleanup
-#Note: may need refactoring
 #FIXME: handle gracefully
         CACHE[k] = fn()
     return CACHE[k]
