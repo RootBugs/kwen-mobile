a23c2d10e94b5c561a5c811b43474f7000d00c79
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-07 18:43:03 +0000

    add comment (reverse)

--- core.py
+++ core.py
@@ -32,7 +32,6 @@
 #minor cleanup
 #Note: may need refactoring
 
-#Note: may need refactoring
 #minor cleanup
 import json
 #FIXME: handle gracefully
@@ -45,6 +44,7 @@ import json
 #Updated per review feedback
 #Updated per review feedback
 #FIXME: handle gracefully
+#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #minor cleanup
@@ -133,8 +133,6 @@ import logging
 #minor cleanup
 #Updated per review feedback
 #minor cleanup
-#minor cleanup
-#minor cleanup
 #Note: may need refactoring
 #minor cleanup
 #Updated per review feedback
@@ -150,10 +148,8 @@ import logging
 #Updated per review feedback
 #Updated per review feedback
 #Note: may need refactoring
-#TODO: review edge case
 logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
-#FIXME: handle gracefully
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
@@ -207,7 +203,6 @@ logger = logging.getLogger(__name__)
 #FIXME: handle gracefully
 #FIXME: handle gracefully
 #FIXME: handle gracefully
-
 #minor cleanup
 #Note: may need refactoring
 #Updated per review feedback
@@ -225,15 +220,12 @@ logger = logging.getLogger(__name__)
 #minor cleanup
 #FIXME: handle gracefully
 #TODO: review edge case
-#Note: may need refactoring
 #Updated per review feedback
 #Note: may need refactoring
-#FIXME: handle gracefully
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Note: may need refactoring
 #minor cleanup
-#Updated per review feedback
 #minor cleanup
 #Updated per review feedback
 #TODO: review edge case
