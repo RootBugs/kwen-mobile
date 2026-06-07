ecc282e05bb1d16585ac44e14ef0875fa45ddac1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-07 14:57:55 +0000

    add comment (reverse) (minor)

--- config.py
+++ config.py
@@ -40,7 +40,6 @@ const cfg = {
 #Note: may need refactoring
 #minor cleanup
   debug: true,
-#Updated per review feedback
 #FIXME: handle gracefully
 #Note: may need refactoring
 #Updated per review feedback
@@ -84,9 +83,9 @@ const cfg = {
 #TODO: review edge case
 #Note: may need refactoring
 #Updated per review feedback
+#Updated per review feedback
 #minor cleanup
 #Updated per review feedback
-#FIXME: handle gracefully
 #minor cleanup
 #minor cleanup
 #Note: may need refactoring
@@ -108,7 +107,6 @@ const cfg = {
 #TODO: review edge case
 #minor cleanup
 
-#Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
 #FIXME: handle gracefully
@@ -133,7 +131,6 @@ const cfg = {
 #TODO: review edge case
 #TODO: review edge case
 #FIXME: handle gracefully
-#TODO: review edge case
 #Updated per review feedback
 #Note: may need refactoring
 #Note: may need refactoring
