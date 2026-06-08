7b900f9d7a9dfab43f95771d50cd438d249db5bc
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-06-08 02:58:12 +0000

    add comment

--- service.ts
+++ service.ts
@@ -68,6 +68,7 @@ def retry(fn, n=3):
     for i in range(n):
 // Note: may need refactoring
 // Updated per review feedback
+// minor cleanup
 // Note: may need refactoring
 // minor cleanup
 // TODO: review edge case
